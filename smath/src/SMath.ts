5e9bb884a19daf19a420d2570c63ce59c8c3edc0
nicfv <nfmventura@gmail.com>
2024-03-05 09:52:49 -0800

    Add examples for Smath

--- smath/src/SMath.ts
+++ smath/src/SMath.ts
@@ -6,6 +6,11 @@ export abstract class SMath {
      * Determine if a value is numeric.
      * @param n Any value to check
      * @returns True if `n` is a number
+     * @example
+     * ```js
+     * const b1 = SMath.isNumber(2);   // true
+     *       b2 = SMath.isNumber('2'); // false
+     * ```
      */
     public static isNumber(n: any): boolean {
         return typeof n === 'number';
@@ -16,6 +21,11 @@ export abstract class SMath {
      * @param min The minimum value of the range
      * @param max The maximum value of the range
      * @returns A clamped number
+     * @example
+     * ```js
+     * const n1 = SMath.clamp(5, 0, 10),  // 5
+     *       n2 = SMath.clamp(-2, 0, 10); // 0
+     * ```
      */
     public static clamp(n: number, min: number, max: number): number {
         if (n < min) {
@@ -32,6 +42,11 @@ export abstract class SMath {
      * @param b Any number
      * @param epsilon Maximum absolute error
      * @returns True if `a` is approximately `b`
+     * @example
+     * ```js
+     * const b1 = SMath.approx(1 / 3, 0.33, 1e-6), // false
+     *       b2 = SMath.approx(1 / 3, 0.33, 1e-2); // true
+     * ```
      */
     public static approx(a: number, b: number, epsilon: number = 1e-6): boolean {
         return a - b < epsilon && b - a < epsilon;
