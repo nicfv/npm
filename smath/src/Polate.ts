73a63d3556d450526d972d969c368dfe43a036d3
nicfv <nfmventura@gmail.com>
2024-03-05 09:42:43 -0800

    Add examples (polate)

--- smath/src/Polate.ts
+++ smath/src/Polate.ts
@@ -8,6 +8,10 @@ export abstract class Polate {
      * @param min The minimum value in the range
      * @param max The maximum value in the range
      * @returns A normalized value
+     * @example
+     * ```js
+     * const y = Polate.normalize(18, 9, 99); // 0.1
+     * ```
      */
     public static normalize(n: number, min: number, max: number): number {
         if (min === max) {
@@ -21,6 +25,10 @@ export abstract class Polate {
      * @param min The minimum value in the range
      * @param max The maximum value in the range
      * @returns A value within the number range
+     * @example
+     * ```js
+     * const y = Polate.expand(0.25, 4, 6); // 4.5
+     * ```
      */
     public static expand(n: number, min: number, max: number): number {
         return (max - min) * n + min;
@@ -33,6 +41,10 @@ export abstract class Polate {
      * @param min2 The minimum value for the final range
      * @param max2 The maximum value for the final range
      * @returns A translated number in the final range
+     * @example
+     * ```js
+     * const C = 20, F = Polate.translate(C, 0, 100, 32, 212); // 68
+     * ```
      */
     public static translate(n: number, min1: number, max1: number, min2: number, max2: number): number {
         return this.expand(this.normalize(n, min1, max1), min2, max2);
