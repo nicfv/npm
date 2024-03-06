bf690c3780f9ebffd1efc9bdf59397c39210deab
nicfv <nfmventura@gmail.com>
2024-03-06 11:43:46 -0800

    Add examples for gradient

--- viridis/src/Gradient.ts
+++ viridis/src/Gradient.ts
@@ -7,6 +7,13 @@ export class Gradient {
     /**
      * Define a new linear color gradient using an array of color stops.
      * @param colors An array of colors to define this color gradient
+     * @example
+     * ```js
+     * const redBlue = new Gradient([
+     *     new Color(255, 0, 0),
+     *     new Color(0, 0, 255),
+     * ]);
+     * ```
      */
     constructor(private readonly colors: Array<Color>) {
         if (colors.length < 1) {
@@ -17,6 +24,11 @@ export class Gradient {
      * Linearly interpolate between color stops to get a color along this gradient.
      * @param x A normalized value to use for selecting a color along this gradient
      * @returns The interpolated color
+     * @example
+     * ```js
+     * const myColor = redBlue.getColor(0.5);
+     * // Returns interpolated color (127.5, 0, 127.5)
+     * ```
      */
     public getColor(x: number): Color {
         if (this.colors.length === 1) {
@@ -43,6 +55,11 @@ export class Gradient {
      * Return a string representation of this gradient.
      * @param deg The direction of this color gradient, in degrees
      * @returns A valid CSS color gradient
+     * @example
+     * ```js
+     * const str = redBlue.toString(180);
+     * // linear-gradient(180deg,rgba(255,0,0,100%),rgba(0,0,255,100%))
+     * ```
      */
     public toString(deg: number = 90): string {
         return 'linear-gradient(' + deg + 'deg,' + this.colors.map(color => color.toString()).join(',') + ')';
