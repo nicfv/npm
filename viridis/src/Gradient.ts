1402b54066be13312e36b5868e0dc50043996012
nicfv <nfmventura@gmail.com>
2024-03-05 16:10:52 -0800

    Uniform color gradient, no stops

--- viridis/src/Gradient.ts
+++ viridis/src/Gradient.ts
@@ -1,38 +1,42 @@
 import { SMath } from 'smath';
 import { Color } from './Color';
-
 /**
- * Represents a single color stop in a linear gradient.
+ * Represents a linear, uniform color gradient.
  */
-export class ColorStop {
-    /**
-     * Define a new color stop.
-     * @param color The color of the color stop
-     * @param stop The normalized value [0-1] for the location of the color stop
-     */
-    constructor(public readonly color: Color, public readonly stop: number) {
-        this.stop = SMath.clamp(stop, 0, 1);
-    }
-}
-
 export class Gradient {
-    constructor(protected readonly colorStops: Array<ColorStop>) {
-        if (colorStops.length < 2) {
-            throw new Error('Must include at least 2 colors to create a gradient.');
+    /**
+     * Define a new linear color gradient using an array of color stops.
+     * @param colors An array of colors to define this color gradient
+     */
+    constructor(protected readonly colors: Array<Color>) {
+        if (colors.length < 1) {
+            throw new Error('Must include at least 1 color to create a gradient.');
         }
-        this.colorStops.sort((a, b) => a.stop - b.stop);
     }
-    public static linear(colors: Array<Color>): Gradient {
-        const N: number = colors.length - 1;
-        return new Gradient(colors.map((color, i) => new ColorStop(color, SMath.translate(i, 0, N, 0, 1))));
-    }
+    /**
+     * Linearly interpolate between color stops to get a color along this gradient.
+     * @param x A normalized value to use for selecting a color along this gradient
+     * @returns The interpolated color
+     */
     public getColor(x: number): Color {
-        const N: number = this.colorStops.length - 1;
-        if (x <= this.colorStops[0].stop) {
-            return this.colorStops[0].color;
-        } else if (x >= this.colorStops[N].stop) {
-            return this.colorStops[N].color;
+        if (this.colors.length === 1) {
+            return this.colors[0];
+        } else if (x <= 0) {
+            return this.colors[0];
+        } else if (x >= 1) {
+            return this.colors[this.colors.length - 1];
         }
-        throw 'TODO';
+        const N = this.colors.length - 1, // number of buckets
+            s = 1 / N, // size of each bucket
+            n = Math.floor(x / s), // bucket number
+            a = s * n, // bucket min value
+            b = s * (n + 1), // bucket max value
+            A = this.colors[n], // bucket min color
+            B = this.colors[n + 1]; //bucket max color
+        return new Color(
+            SMath.translate(x, a, b, A.red, B.red),
+            SMath.translate(x, a, b, A.green, B.green),
+            SMath.translate(x, a, b, A.blue, B.blue),
+            SMath.translate(x, a, b, A.alpha, B.alpha));
     }
 }
