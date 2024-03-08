2a233c0dd8e6ecb4ce3d97eb48f222cd33e2a20a
nicfv <nfmventura@gmail.com>
2024-03-08 15:44:33 -0800

    Add types for single or multi variable problems

--- datafit/src/types.ts
+++ datafit/src/types.ts
@@ -1,15 +1,27 @@
 /**
+ * Use this to define a single-variable curve and dataset
+ */
+export type SingleVariable = number;
+/**
+ * Use this to define a multi-variable curve and dataset
+ */
+export type MultiVariable = Array<number>;
+/**
+ * Defines whether this is a single- or multi-variable problem.
+ */
+export type X = SingleVariable | MultiVariable;
+/**
  * Represents a mathematical function y = f(x) with unknown constants `a`
  */
-export type fx = (x: number, ...a: Array<number>) => number;
+export type fx<T = X> = (x: T, ...a: Array<number>) => number;
 /**
  * Stores a cartesian (x,y) coordinate pair.
  */
-export interface Point {
+export interface Point<T = X> {
     /**
      * X-coordinate
      */
-    readonly x: number;
+    readonly x: T;
     /**
      * Y-coordinate
      */
