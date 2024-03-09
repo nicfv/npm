892fb06b8ea96d98bc42fd4f3a1017f8c245b7b9
nicfv <nfmventura@gmail.com>
2024-03-08 22:17:43 -0800

    Clean up config, show default configuration settings

--- datafit/src/lib.ts
+++ datafit/src/lib.ts
@@ -27,6 +27,17 @@ const defaultConfig: Config = {
  *     params: Params = bestFit.params,
  *     err: number = bestFit.err;
  * ```
+ * @default
+ * Configuration settings
+ * ```js
+ * const defaultConfig: Config = {
+ *     generations: 100,
+ *     population: 100,
+ *     survivors: 10,
+ *     initialDeviation: 10,
+ *     finalDeviation: 1,
+ * };
+ * ```
  */
 export function fit<T = VariableType>(f: fx<T>, data: Dataset<T>, params_initial: Params = [], config: Config = defaultConfig): Fit {
     const N_params: number = f.length - 1;
@@ -37,19 +48,27 @@ export function fit<T = VariableType>(f: fx<T>, data: Dataset<T>, params_initial
     if (params_initial.length !== N_params) {
         throw new Error('The initial guess should contain ' + N_params + ' parameters.');
     }
+    // Clean up a potentially incomplete config object by filling it in with default options
+    const conf: Config = {
+        generations: config.generations ?? defaultConfig.generations,
+        population: config.population ?? defaultConfig.population,
+        survivors: config.survivors ?? defaultConfig.survivors,
+        initialDeviation: config.initialDeviation ?? defaultConfig.initialDeviation,
+        finalDeviation: config.finalDeviation ?? defaultConfig.finalDeviation,
+    };
     const census: Array<Fit> = [];
-    for (let generation = 0; generation < config.generations ?? defaultConfig.generations; generation++) {
-        for (let i = 0; i < config.population ?? defaultConfig.population; i++) {
+    for (let generation = 0; generation < conf.generations; generation++) {
+        for (let i = 0; i < conf.population; i++) {
             // Mutate a random parent from the prior generation of survivors
             const params: Params = mutate(
-                census[randInt(0, config.survivors ?? defaultConfig.survivors)]?.params ?? params_initial,
-                SMath.translate(generation, 0, config.generations ?? defaultConfig.generations, config.initialDeviation ?? defaultConfig.initialDeviation, config.finalDeviation ?? defaultConfig.finalDeviation)
+                census[randInt(0, conf.survivors)]?.params ?? params_initial,
+                SMath.translate(generation, 0, conf.generations, conf.initialDeviation, conf.finalDeviation)
             );
             census.push({ params: params, err: err(f, params, data) });
         }
         // Sort by increasing error and only keep the survivors
         census.sort((x, y) => x.err - y.err);
-        census.splice(config.survivors ?? defaultConfig.survivors);
+        census.splice(conf.survivors);
     }
     return census[0];
 }
