25c0e1d2c12424c594bace9731160aa0ffcedfad
nicfv <envneo+github@gmail.com>
2024-03-20 20:08:35 -0700

    Add comment

--- smath/examples/comparison.ts
+++ smath/examples/comparison.ts
@@ -1,4 +1,5 @@
 import { SMath } from 'smath';
 
+// Determine the value of 0.1 + 0.2 using vanilla JavaScript and SMath
 console.log('0.1 + 0.2 == 0.3 is ' + (0.1 + 0.2 == 0.3));
 console.log('SMath.approx(0.1 + 0.2, 0.3) is ' + SMath.approx(0.1 + 0.2, 0.3));
