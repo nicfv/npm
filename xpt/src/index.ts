ad8f461f1db6b1f2256ebb2cb219a10b2ecde8fb
nicfv <envneo+github@gmail.com>
2024-03-22 04:03:14 -0700

    Add eq(), false(). need to add messages

--- xpt/src/index.ts
+++ xpt/src/index.ts
@@ -12,7 +12,7 @@
 export abstract class xpt {
     /**
      * Expect a test to return true.
-     * @param test A boolean test
+     * @param test A test that returns a boolean result
      * @param message The exception message, if found
      */
     public static true(test: boolean, message: string = 'Exception found! Value was ' + test + '.'): void {
@@ -21,12 +21,27 @@ export abstract class xpt {
         }
     }
     /**
+     * Expect a test to return false.
+     * @param test A test that returns a boolean result
+     */
+    public static false(test: boolean): void {
+        this.true(!test);
+    }
+    /**
      * Expect two arguments of any type to contain the same value(s).
      * @param arg1 Argument 1, any value is accepted
      * @param arg2 Argument 2, any value is accepted
      */
-    public static is(arg1: any, arg2: any) {
+    public static is(arg1: any, arg2: any): void {
         this.true(JSON.stringify(arg1) === JSON.stringify(arg2));
+    }
+    /**
+     * Expect two numbers to be of equal value.
+     * @param num1 Numeric input 1
+     * @param num2 Numeric input 2
+     */
+    public static eq(num1: number, num2: number): void {
+        this.true(num1 === num2);
     }
 }
 /**
