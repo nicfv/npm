5b5916f32b63d9ba09a0bffdca5f97bfa3fa4f9d
nicfv <envneo+github@gmail.com>
2024-03-22 11:19:27 -0700

    Add tests for normalize and translate

--- smath/test/index.ts
+++ smath/test/index.ts
@@ -21,3 +21,14 @@ xpt.eq(SMath.expand(0, 4, 8), 4);
 xpt.eq(SMath.expand(0.5, 4, 8), 6);
 xpt.eq(SMath.expand(1, 4, 8), 8);
 xpt.eq(SMath.expand(2, 4, 8), 12);
+
+xpt.eq(SMath.normalize(8, 10, 12), -1);
+xpt.eq(SMath.normalize(10, 10, 12), 0);
+xpt.eq(SMath.normalize(11, 10, 12), 0.5);
+xpt.eq(SMath.normalize(12, 10, 12), 1);
+xpt.eq(SMath.normalize(14, 10, 12), 2);
+
+xpt.eq(SMath.translate(20, 0, 100, 32, 212), 68);
+xpt.eq(SMath.translate(-40, 0, 100, 32, 212), -40);
+xpt.eq(SMath.translate(68, 32, 212, 0, 100), 20);
+xpt.eq(SMath.translate(-40, 32, 212, 0, 100), -40);
