80d606a8081c26c5314df57918f6e0bc2ccd205f
Nic <24358775+nicfv@users.noreply.github.com>
2024-03-22 04:33:48 -0700

    Xpt (#69)

--- xpt/CHANGELOG.md
+++ xpt/CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
+## 0.1.0
+
+- Update project tagline
+- Update getting started in readme
+- Remove how internal testing works (everything uses the public-facing `true()` test)
+- Remove `is()` (inconsistencies with using `JSON.stringify`)
+- Add `true()`, `false()`, `eq()`, and `ne()`
+
 ## 0.0.1
 
 - Set up project package
--- xpt/src/index.ts
+++ xpt/src/index.ts
@@ -1,33 +1,71 @@
 /**
  * @packageDocumentation
- * Finds exceptions in your code
+ * Expect or except!
  * 
  * Exports the public-facing API for `xpt`
  */
 /**
- * Framework to find exceptions.
+ * Lightweight framework to expect
+ * results or throw exceptions. If
+ * any test fails, halts execution.
+ * Exceptions can be caught with
+ * standard `try ... catch` blocks.
  */
 export abstract class xpt {
     /**
-     * Detect an exception for a failed test.
-     * @param pass Whether the assertion will pass or find an exception.
-     * @param message The exception message, if found.
+     * Expect a test to return true.
+     * @param test A test that returns a boolean result
+     * @param message The exception message to show if
+     * an unexpected result was found. If not set, will
+     * display a default message for this type of test.
      */
-    private static assert(pass: boolean, message: string = 'Exception found!'): void {
-        if (!pass) {
+    public static true(test: boolean, message: string = 'Test failed. Value was ' + test + '.'): void {
+        if (!test) {
             throw new Exception(message);
         }
     }
     /**
-     * Test that `a` is `b` or throw an exception.
-     * @param a Any value
-     * @param b Any value
+     * Expect a test to return false.
+     * @param test A test that returns a boolean result
+     * @param message The exception message to show if
+     * an unexpected result was found. If not set, will
+     * display a default message for this type of test.
      */
-    public static is(a: any, b: any) {
-        this.assert(JSON.stringify(a) === JSON.stringify(b));
+    public static false(test: boolean, message: string = 'Test failed. Value was ' + test + '.'): void {
+        this.true(!test, message);
+    }
+    /**
+     * Expect two numbers to be of equal value.
+     * @param num1 Numeric input 1
+     * @param num2 Numeric input 2
+     * @param message The exception message to show if
+     * an unexpected result was found. If not set, will
+     * display a default message for this type of test.
+     */
+    public static eq(num1: number, num2: number, message: string = 'Numbers ' + num1 + ' and ' + num2 + ' are not equal.'): void {
+        this.true(num1 === num2, message);
+    }
+    /**
+     * Expect two numbers to be of different values.
+     * @param num1 Numeric input 1
+     * @param num2 Numeric input 2
+     * @param message The exception message to show if
+     * an unexpected result was found. If not set, will
+     * display a default message for this type of test.
+     */
+    public static ne(num1: number, num2: number, message: string = 'Numbers ' + num1 + ' and ' + num2 + ' are equal.'): void {
+        this.true(num1 !== num2, message);
     }
 }
 /**
  * Exceptions extend the base `Error` class.
  */
-class Exception extends Error { }
+class Exception extends Error {
+    /**
+     * Create a new exception to throw.
+     * @param message The exception message
+     */
+    constructor(message: string) {
+        super('Exception found! ' + message);
+    }
+}
