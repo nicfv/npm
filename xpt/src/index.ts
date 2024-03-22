720461ba1f014ebb4631183f9e2a034af5ec6a06
nicfv <envneo+github@gmail.com>
2024-03-22 11:03:23 -0700

    expected -> expect

--- xpt/src/index.ts
+++ xpt/src/index.ts
@@ -37,90 +37,90 @@ export abstract class xpt {
     /**
      * Expect the test string to be **identical** to the expected string.
      * @param test A test that returns a string result
-     * @param expected The expected string to use for comparison
+     * @param expect The expected string to use for comparison
      * @param message The exception message to show if
      * an unexpected result was found. If not set, will
      * display a default message for this type of test.
      */
-    public static is(test: string, expected: string, message: string = 'The test string "' + test + '" did not match the expected string "' + expected + '".'): void {
-        this.true(test === expected, message);
+    public static is(test: string, expect: string, message: string = 'The test string "' + test + '" did not match the expected string "' + expect + '".'): void {
+        this.true(test === expect, message);
     }
     /**
      * Expect the test string to be **different** than the expected string.
      * @param test A test that returns a string result
-     * @param expected The expected string to use for comparison
+     * @param expect The expected string to use for comparison
      * @param message The exception message to show if
      * an unexpected result was found. If not set, will
      * display a default message for this type of test.
      */
-    public static not(test: string, expected: string, message: string = 'The test string "' + test + '" matched the expected string "' + expected + '".'): void {
-        this.true(test !== expected, message);
+    public static not(test: string, expect: string, message: string = 'The test string "' + test + '" matched the expected string "' + expect + '".'): void {
+        this.true(test !== expect, message);
     }
     /**
      * Expect the test to **be equal** to the expected value.
      * @param test A test that returns a numeric result
-     * @param expected The expected number to use for comparison
+     * @param expect The expected number to use for comparison
      * @param message The exception message to show if
      * an unexpected result was found. If not set, will
      * display a default message for this type of test.
      */
-    public static eq(test: number, expected: number, message: string = 'The test value ' + test + ' was not equal to the expected value ' + expected + '.'): void {
-        this.true(test === expected, message);
+    public static eq(test: number, expect: number, message: string = 'The test value ' + test + ' was not equal to the expected value ' + expect + '.'): void {
+        this.true(test === expect, message);
     }
     /**
      * Expect the test to **not be equal** to the expected value.
      * @param test A test that returns a numeric result
-     * @param expected The expected number to use for comparison
+     * @param expect The expected number to use for comparison
      * @param message The exception message to show if
      * an unexpected result was found. If not set, will
      * display a default message for this type of test.
      */
-    public static ne(test: number, expected: number, message: string = 'The test value ' + test + ' was equal to the expected value ' + expected + '.'): void {
-        this.true(test !== expected, message);
+    public static ne(test: number, expect: number, message: string = 'The test value ' + test + ' was equal to the expected value ' + expect + '.'): void {
+        this.true(test !== expect, message);
     }
     /**
      * Expect the test to be strictly **greater than** the expected value.
      * @param test A test that returns a numeric result
-     * @param expected The expected number to use for comparison
+     * @param expect The expected number to use for comparison
      * @param message The exception message to show if
      * an unexpected result was found. If not set, will
      * display a default message for this type of test.
      */
-    public static gt(test: number, expected: number, message: string = 'The test value ' + test + ' was not greater than the expected value ' + expected + '.'): void {
-        this.true(test > expected, message);
+    public static gt(test: number, expect: number, message: string = 'The test value ' + test + ' was not greater than the expected value ' + expect + '.'): void {
+        this.true(test > expect, message);
     }
     /**
      * Expect the test to be strictly **less than** the expected value.
      * @param test A test that returns a numeric result
-     * @param expected The expected number to use for comparison
+     * @param expect The expected number to use for comparison
      * @param message The exception message to show if
      * an unexpected result was found. If not set, will
      * display a default message for this type of test.
      */
-    public static lt(test: number, expected: number, message: string = 'The test value ' + test + ' was not less than the expected value ' + expected + '.'): void {
-        this.true(test < expected, message);
+    public static lt(test: number, expect: number, message: string = 'The test value ' + test + ' was not less than the expected value ' + expect + '.'): void {
+        this.true(test < expect, message);
     }
     /**
      * Expect the test to be **greater than or equal to** the expected value.
      * @param test A test that returns a numeric result
-     * @param expected The expected number to use for comparison
+     * @param expect The expected number to use for comparison
      * @param message The exception message to show if
      * an unexpected result was found. If not set, will
      * display a default message for this type of test.
      */
-    public static ge(test: number, expected: number, message: string = 'The test value ' + test + ' was not greater than nor equal to the expected value ' + expected + '.'): void {
-        this.true(test >= expected, message);
+    public static ge(test: number, expect: number, message: string = 'The test value ' + test + ' was not greater than nor equal to the expected value ' + expect + '.'): void {
+        this.true(test >= expect, message);
     }
     /**
      * Expect the test to be **less than or equal to** the expected value.
      * @param test A test that returns a numeric result
-     * @param expected The expected number to use for comparison
+     * @param expect The expected number to use for comparison
      * @param message The exception message to show if
      * an unexpected result was found. If not set, will
      * display a default message for this type of test.
      */
-    public static le(test: number, expected: number, message: string = 'The test value ' + test + ' was not less than nor equal to the expected value ' + expected + '.'): void {
-        this.true(test <= expected, message);
+    public static le(test: number, expect: number, message: string = 'The test value ' + test + ' was not less than nor equal to the expected value ' + expect + '.'): void {
+        this.true(test <= expect, message);
     }
 }
 /**
