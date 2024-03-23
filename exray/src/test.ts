29bd1add27dbc9849261949204398809f89a0efe
nicfv <envneo+github@gmail.com>
2024-03-23 13:44:21 -0700

    Fix syntax errors in test

--- exray/src/test.ts
+++ exray/src/test.ts
@@ -1,16 +1,17 @@
 import { X } from './index';
 
 X.true(true);
+X.false(false);
 X.eq(1, 1);
 
 try {
     X.true(false);
 } catch (e) {
-    X.is(e.message, 'Exception found in test #3! The test returned false.');
+    X.is((e as Error).message, 'Exception found in test #3! The test returned false.');
 }
 
 try {
     X.ne(1, 1, 'Custom');
 } catch (e) {
-    X.is(e.message, 'Exception found in test #5! Custom');
+    X.is((e as Error).message, 'Exception found in test #5! Custom');
 }
