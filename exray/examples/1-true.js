84e950995642bf823a718f1b0993244d5e85eb73
nicfv <envneo+github@gmail.com>
2024-03-22 21:59:51 -0700

    Add examples for exray

---
+++ exray/examples/1-true.js
@@ -0,0 +1,18 @@
+import { X } from 'exray';
+
+// This function always returns true.
+function alwaysReturnsTrue() {
+    return true;
+}
+// This test should pass
+X.true(alwaysReturnsTrue());
+
+// This function always returns false.
+function alwaysReturnsFalse() {
+    return false;
+}
+// This test should pass
+X.false(alwaysReturnsFalse());
+
+// This test should fail
+X.true(alwaysReturnsFalse());
---
+++ exray/examples/4-message.js
@@ -0,0 +1,28 @@
+import { X } from 'exray';
+
+// Here's a simulation of
+// an API to obtain the
+// list price of an item.
+function getPrice() {
+    return 50;
+}
+
+// Let's say our cap is $40 to
+// continue processing. We can
+// assert that the price is less
+// than or equal to $40 or throw
+// an error. For this example,
+// we will catch the exception.
+try {
+    X.le(getPrice(), 40);
+} catch (e) {
+    console.log(e.message);
+}
+
+// Here is the same logic using
+// a custom exception message.
+try {
+    X.le(getPrice(), 40, 'Price exceeded $40 limit.');
+} catch (e) {
+    console.log(e.message);
+}
