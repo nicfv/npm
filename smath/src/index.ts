9e8c473878f4175968271bbf4142491ddc1b4b8e
Nicolas Ventura <ventura@lbl.gov>
2024-03-24 19:24:08 -0700

    Update examps

--- smath/src/index.ts
+++ smath/src/index.ts
@@ -53,7 +53,7 @@ export abstract class SMath {
      * @returns The population variance
      * @example
      * ```js
-     * const pvar = SMath.pvar(1, 2, 3, 4); // TODO: UPDATE AND WRITE TESTS
+     * const pvar = SMath.pvar(1, 2, 3, 4); // 1.25
      * ```
      */
     public static pvar(...n: Array<number>): number {
@@ -67,7 +67,7 @@ export abstract class SMath {
      * @returns The sample variance
      * @example
      * ```js
-     * const svar = SMath.svar(1, 2, 3, 4); // TODO
+     * const svar = SMath.svar(1, 2, 3, 4); // 1.666...
      * ```
      */
     public static svar(...n: Array<number>): number {
