32f6767000cdbe281fcc3edc8b18b0500749f9d0
nicfv <envneo+github@gmail.com>
2024-04-05 15:14:08 -0700

    Add nonlinear/magnitude test

--- datafit/src/test.ts
+++ datafit/src/test.ts
@@ -52,3 +52,27 @@ X.le(Math.abs(SMath.error(summary2.params[0], b[0])), tolerance);
 X.le(Math.abs(SMath.error(summary2.params[1], b[1])), tolerance);
 X.le(Math.abs(SMath.error(summary2.params[2], b[2])), tolerance);
 X.le(Math.abs(SMath.error(summary2.f([-5, -5]), g([-5, -5]))), tolerance);
+
+// Define a nonlinear function with wildly different parameter magnitudes
+const c: Array<number> = [1000, -200, 1];
+function h(x: number, c0: number = c[0], c1: number = c[1], c2: number = c[2]): number {
+    return (1 + Math.abs(c2)) ** x + c1 * x + c0;
+}
+// Generate dataset and fit curve
+const data3: Datum<number>[] = SMath.linspace(0, 10, 11).map(x => ({ x: x, y: h(x) })),
+    summary3: Summary<number> = fit(h, data3);
+// Make sure dataset is accurate
+X.eq(data3[0].x, 0);
+X.eq(data3[0].y, 1001);
+X.eq(data3[5].x, 5);
+X.eq(data3[5].y, 32);
+X.eq(data3[8].x, 8);
+X.eq(data3[8].y, -344);
+X.eq(data3[10].x, 10);
+X.eq(data3[10].y, 24);
+// Validate accuracy of fitted data
+X.le(Math.abs(SMath.error(summary3.params[0], c[0])), tolerance);
+X.le(Math.abs(SMath.error(summary3.params[1], c[1])), tolerance);
+X.le(Math.abs(SMath.error(Math.abs(summary3.params[2]), c[2])), tolerance);
+X.le(Math.abs(SMath.error(summary3.f(0), h(0))), tolerance);
+X.le(Math.abs(SMath.error(summary3.f(8), h(8))), tolerance);
