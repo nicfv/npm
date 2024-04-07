4370c21ea93b8d7757c5cbe32902ba98eea5618f
nicfv <envneo+github@gmail.com>
2024-04-07 14:06:23 -0700

    tsdoc

--- dimensional/src/compound.ts
+++ dimensional/src/compound.ts
@@ -1,10 +1,18 @@
 import { SMath } from 'smath';
-import { Dictionary, NumberDictionary } from './lib';
+import { NumberDictionary } from './lib';
 
+/**
+ * Represents a compound unit or dimension.
+ */
 export class Compound<T extends string> {
     private readonly num: NumberDictionary<T> = {};
     private readonly den: NumberDictionary<T> = {};
-    constructor(private readonly exponents: NumberDictionary<T>, private readonly dictionary: Dictionary<T>) {
+    /**
+     * Create a new compound unit or dimension.
+     * @param exponents Contains an object of exponent values
+     * @param toLaTeX A function to convert the exponent to its representation in LaTeX
+     */
+    constructor(private readonly exponents: NumberDictionary<T>, private readonly toLaTeX: (exponent: T) => string) {
         for (const t in exponents) {
             const exponent: number = exponents[t] ?? 0;
             if (exponent > 0) {
@@ -14,6 +22,12 @@ export class Compound<T extends string> {
             }
         }
     }
+    /**
+     * Combine two compounds by applying a factor on the second compound.
+     * @param other Another compount
+     * @param factor The factor to use on the other compound
+     * @returns The combination of the two compounds
+     */
     private combine(other: Compound<T>, factor: 1 | -1): Compound<T> {
         const exponents_combined: NumberDictionary<T> = {};
         for (const t in this.exponents) {
@@ -22,14 +36,29 @@ export class Compound<T extends string> {
         for (const t in other.exponents) {
             exponents_combined[t] = (exponents_combined[t] ?? 0) + factor * (other.exponents[t] ?? 0);
         }
-        return new Compound<T>(exponents_combined, this.dictionary);
+        return new Compound<T>(exponents_combined, this.toLaTeX);
     }
+    /**
+     * Multiply this compound by another.
+     * @param other Another compound
+     * @returns The product
+     */
     public mult(other: Compound<T>): Compound<T> {
         return this.combine(other, 1);
     }
+    /**
+     * Divide this compound by another.
+     * @param other Another compound
+     * @returns The dividend
+     */
     public div(other: Compound<T>): Compound<T> {
         return this.combine(other, -1);
     }
+    /**
+     * Determine whether two compounds contain the same units or dimensions.
+     * @param other Another compound
+     * @returns A boolean
+     */
     public is(other: Compound<T>): boolean {
         const dividend: Compound<T> = this.div(other);
         for (let t in dividend.exponents) {
@@ -40,20 +69,29 @@ export class Compound<T extends string> {
         }
         return true;
     }
+    /**
+     * Generate LaTeX code for a number dictionary.
+     * @param dict Any number dictionary
+     * @returns Partial LaTeX code
+     */
     private prettyPrint(dict: NumberDictionary<T>): string {
         let str: string = '';
         for (const t in dict) {
             const exponent: number = dict[t] ?? 0;
             if (SMath.approx(exponent, 1)) {
-                str += this.dictionary[t];
+                str += this.toLaTeX(t);
             } else if (SMath.approx(exponent, 0.5)) {
-                str += '\\sqrt{' + this.dictionary[t] + '}';
+                str += '\\sqrt{' + this.toLaTeX(t) + '}';
             } else {
-                str += this.dictionary[t] + '^{' + exponent.toString() + '}';
+                str += this.toLaTeX(t) + '^{' + exponent.toString() + '}';
             }
         }
         return str;
     }
+    /**
+     * Generate valid LaTeX code representing this compound.
+     * @returns A valid LaTeX equation
+     */
     public toString(): string {
         let str: string = '';
         const hasNum: boolean = Object.keys(this.num).length > 0,
