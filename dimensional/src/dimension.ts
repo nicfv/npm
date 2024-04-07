40b87f5ffeab3a0e05e67359be4373d892668ff3
nicfv <envneo+github@gmail.com>
2024-04-07 14:15:12 -0700

    Cleanup

--- dimensional/src/dimension.ts
+++ dimensional/src/dimension.ts
@@ -1,25 +1,27 @@
 import { Compound } from './compound';
-import { NumberDictionary } from './lib';
+import { Dictionary, NumberDictionary } from './lib';
 
 /**
  * A list of common names for each of the physical base dimensions.
  */
 export type Dimensions = 'time' | 'length' | 'mass' | 'current' | 'temperature' | 'amount' | 'intensity';
-
+/**
+ * Contains all physical base dimensions and their corresponding abbreviations.
+ */
+const dimensionTable: Dictionary<Dimensions> = {
+    'amount': 'N',
+    'current': 'I',
+    'intensity': 'J',
+    'length': 'L',
+    'mass': 'M',
+    'temperature': '\\Theta',
+    'time': 'T',
+};
+/**
+ * Defines the class for physical base dimensions.
+ */
 export class Dimension extends Compound<Dimensions> {
     constructor(exponents: NumberDictionary<Dimensions>) {
-        super(exponents, t => ({
-            'time': 'T',
-            'length': 'L',
-            'mass': 'M',
-            'current': 'I',
-            'temperature': '\\Theta',
-            'amount': 'N',
-            'intensity': 'J',
-        })[t]);
+        super(exponents, t => dimensionTable[t]);
     }
 }
-
-const asdf = { 'a': 'b', 'c': 'd' };
-
-type x = keyof (typeof asdf);
