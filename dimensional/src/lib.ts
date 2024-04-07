b8d5037ef66d88b3f9b306ae5809136e36e35f45
nicfv <envneo+github@gmail.com>
2024-04-07 14:06:32 -0700

    remove dictionary

--- dimensional/src/lib.ts
+++ dimensional/src/lib.ts
@@ -1,8 +1,4 @@
 /**
- * Represents a type where every object value is a string.
- */
-export type Dictionary<T extends string> = { [index in T]: string };
-/**
  * Represents a type where every object value is a number.
  */
 export type NumberDictionary<T extends string> = { [index in T]?: number };
