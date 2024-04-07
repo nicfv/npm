802bebd57e4dd99fd197204c55de2f5e6baa1281
nicfv <envneo+github@gmail.com>
2024-04-07 14:52:20 -0700

    Mess around in testing

--- dimensional/src/test.ts
+++ dimensional/src/test.ts
@@ -1,5 +1,25 @@
 import { Dimension } from './dimension';
+import { Unit } from './unit';
 
-const V = new Dimension({ 'length': -1, 'time': -1 });
+const V = new Dimension({});
 const speed = new Dimension({ 'time': -1, 'length': -1 })
-console.log(V.toString(), speed.toString(), V.is(speed));
+console.log(V.toString(), speed.toString(), V.is(speed), V.getExponent('amount'), V.getExponent('time'));
+
+const hz = new Unit({ 'seconds': -1 });
+console.log(hz.toString());
+
+class Parent {
+    public func(): void {
+        console.log('hello!');
+    }
+}
+
+class Child extends Parent {
+    public func(): void {
+        super.func();
+        console.log('world!');
+    }
+}
+
+const c = new Child();
+c.func();
