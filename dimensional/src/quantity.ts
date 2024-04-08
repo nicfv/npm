0808e84d39da0c1c01b7b71b2551966cb985bb78
nicfv <envneo+github@gmail.com>
2024-04-07 17:20:34 -0700

    Use scale, expand, and normalize (SMath)

--- dimensional/src/quantity.ts
+++ dimensional/src/quantity.ts
@@ -7,12 +7,7 @@ export class Quantity {
     constructor(private readonly value: number, private readonly unit: Unit) {
         this.base = value;
         for (const u of unit.getNonzeroExponents()) {
-            const exponent: number = unit.getExponent(u);
-            this.base = SMath.translate(this.base,
-                (ConversionTable[u].this.min === 0) ? 0 : (ConversionTable[u].this.min ** exponent),
-                (ConversionTable[u].this.max === 0) ? 0 : (ConversionTable[u].this.max ** exponent),
-                (ConversionTable[u].base.min === 0) ? 0 : (ConversionTable[u].base.min ** exponent),
-                (ConversionTable[u].base.max === 0) ? 0 : (ConversionTable[u].base.max ** exponent));
+            this.base = SMath.expand(this.base, 0, ConversionTable[u].scale ** unit.getExponent(u));
         }
     }
     public as(newUnit: Unit): Quantity {
@@ -21,12 +16,7 @@ export class Quantity {
         }
         let newValue: number = this.base;
         for (const u of newUnit.getNonzeroExponents()) {
-            const exponent: number = newUnit.getExponent(u);
-            newValue = SMath.translate(newValue,
-                (ConversionTable[u].base.min === 0) ? 0 : (ConversionTable[u].base.min ** exponent),
-                (ConversionTable[u].base.max === 0) ? 0 : (ConversionTable[u].base.max ** exponent),
-                (ConversionTable[u].this.min === 0) ? 0 : (ConversionTable[u].this.min ** exponent),
-                (ConversionTable[u].this.max === 0) ? 0 : (ConversionTable[u].this.max ** exponent));
+            newValue = SMath.normalize(newValue, 0, ConversionTable[u].scale ** newUnit.getExponent(u));
         }
         return new Quantity(newValue, newUnit);
     }
