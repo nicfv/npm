d7bf1f1042eaf398230584f64350e1fe9b78db35
nicfv <envneo+github@gmail.com>
2024-04-07 17:20:12 -0700

    Simplify conversion factors to a single "scale"

--- dimensional/src/conversion.ts
+++ dimensional/src/conversion.ts
@@ -2,67 +2,57 @@ import { Dimension } from './dimension';
 import { Units } from './unit';
 
 /**
- * Represents the full conversion table for all units.
+ * Represents the full conversion table for **absolute** units only.
  */
-export const ConversionTable: { [index in Units]: { base: { min: number, max: number }, this: { min: number, max: number }, latex: string, dim: Dimension } } = {
+export const ConversionTable: { [index in Units]: { scale: number, latex: string, dim: Dimension } } = {
     'days': {
         latex: 'd',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 60 * 60 * 24 },
-        this: { min: 0, max: 1 },
+        scale: 60 * 60 * 24,
     },
     'hours': {
         latex: 'h',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 60 * 60 },
-        this: { min: 0, max: 1 },
+        scale: 60 * 60,
     },
     'milliseconds': {
         latex: 'ms',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 1 },
-        this: { min: 0, max: 1e3 },
+        scale: 1e-3,
     },
     'minutes': {
         latex: 'm',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 60 },
-        this: { min: 0, max: 1 },
+        scale: 60,
     },
     'months': {
         latex: 'M',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 60 * 60 * 24 * 365.25 / 12 },
-        this: { min: 0, max: 1 },
+        scale: 60 * 60 * 24 * 365.25 / 12,
     },
     'nanoseconds': {
         latex: 'ns',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 1 },
-        this: { min: 0, max: 1e9 },
+        scale: 1e-9,
     },
     'none': {
         latex: '-',
         dim: new Dimension({}),
-        base: { min: 0, max: 1 },
-        this: { min: 0, max: 1 },
+        scale: 1,
     },
     'seconds': {
         latex: 's',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 1 },
-        this: { min: 0, max: 1 },
+        scale: 1,
     },
     'weeks': {
         latex: 'w',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 60 * 60 * 24 * 7 },
-        this: { min: 0, max: 1 },
+        scale: 60 * 60 * 24 * 7,
     },
     'years': {
         latex: 'y',
         dim: new Dimension({ time: 1 }),
-        base: { min: 0, max: 60 * 60 * 24 * 365.25 },
-        this: { min: 0, max: 1 },
+        scale: 60 * 60 * 24 * 365.25,
     },
 };
