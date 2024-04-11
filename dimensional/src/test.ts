fe7730d2f61d7de11174ff1284d9312ba2b0b3ae
nicfv <envneo+github@gmail.com>
2024-04-10 17:41:39 -0700

    Add unit tests for dim and unit

--- dimensional/src/test.ts
+++ dimensional/src/test.ts
@@ -1,19 +1,26 @@
-import { Dimension } from './dimension';
-import { Quantity } from './quantity';
-import { Unit } from './unit';
+import { X } from 'exray';
+import { Dim, Dimension } from './dimension';
+import { U, Unit } from './unit';
 
-const V = new Dimension({ 'current': 2 });
-const speed = new Dimension({ 'time': -1, 'length': 1, 'temperature': 0.5 })
-console.log(V.toString(), speed.toString(), V.is(speed), V.getExponent('amount'), V.getExponent('time'));
-console.log(V.mult(speed, -1).toString());
+{
+    const velocity: Dimension = Dim({ length: 1, time: -1 }),
+        distance: Dimension = Dim({ length: 1 }),
+        time: Dimension = Dim({ time: 1 });
+    X.eq(velocity.getExponent('length'), 1);
+    X.eq(velocity.getExponent('current'), 0);
+    X.eq(velocity.getNonzeroExponents().length, 2);
+    X.true(velocity.is(distance.mult(time, -1)));
+    X.false(velocity.is(distance.mult(time, 1)));
+    X.is(velocity.toString(), '\\frac{\\textbf{L}}{\\textbf{T}}')
+}
 
-const hz = new Unit({ 'seconds': -1 });
-console.log(hz.toString());
-
-const q1 = new Quantity(1, new Unit({ 'hours': 2 }));
-console.log(q1.toString());
-console.log(q1.as(new Unit({ 'minutes': 2 })).toString());
-
-const q2 = new Quantity(60, new Unit({ miles: 1, hours: -1 }));
-console.log(q2.toString());
-console.log(q2.as(new Unit({ meters: 1, seconds: -1 })).toString());
+{
+    const meters_per_second: Unit = U({ meters: 1, seconds: -1 }),
+        miles_per_hour: Unit = U({ miles: 1, hours: -1 });
+    X.true(meters_per_second.dimension.is(miles_per_hour.dimension));
+    X.false(meters_per_second.is(miles_per_hour));
+    X.eq(meters_per_second.getNonzeroExponents().length, 2);
+    X.eq(meters_per_second.getExponent('meters'), 1);
+    X.eq(meters_per_second.getExponent('days'), 0);
+    X.is(meters_per_second.toString(), '\\frac{m}{s}');
+}
