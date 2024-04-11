906029b3ea9b50a44e6bdb701bcd0e94919bc14e
nicfv <envneo+github@gmail.com>
2024-04-10 17:28:45 -0700

    Clean up tests

--- dimensional/src/test.ts
+++ dimensional/src/test.ts
@@ -2,28 +2,13 @@ import { Dimension } from './dimension';
 import { Quantity } from './quantity';
 import { Unit } from './unit';
 
-const V = new Dimension({});
+const V = new Dimension({ 'current': 2 });
 const speed = new Dimension({ 'time': -1, 'length': 1, 'temperature': 0.5 })
 console.log(V.toString(), speed.toString(), V.is(speed), V.getExponent('amount'), V.getExponent('time'));
+console.log(V.mult(speed, -1).toString());
 
 const hz = new Unit({ 'seconds': -1 });
 console.log(hz.toString());
-
-class Parent {
-    public func(): void {
-        console.log('hello!');
-    }
-}
-
-class Child extends Parent {
-    public func(): void {
-        super.func();
-        console.log('world!');
-    }
-}
-
-const c = new Child();
-c.func();
 
 const q1 = new Quantity(1, new Unit({ 'hours': 2 }));
 console.log(q1.toString());
