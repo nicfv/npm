136a6b4882bd1deb2c6039157c2af075f8531070
nicfv <envneo+github@gmail.com>
2024-04-12 13:10:07 -0700

    Totally revamp conversion class

--- dimensional/src/conversion.ts
+++ dimensional/src/conversion.ts
@@ -1,83 +1,69 @@
 import { Dimension, DimensionExponents } from './dimension';
 import { Prefix } from './prefix';
-import { Unit, UnitExponents, Units } from './unit';
+import { UnitExponents, Units } from './unit';
 
 /**
  * Contains information on how units should be converted.
  */
-abstract class Conversion {
+export class Conversion {
     /**
      * Create a new unit conversion.
-     * @param latex The LaTeX representation of this unit.
+     * @param latex 
+     * @param scale The scale of this unit in relation to the base units that make up this dimension.
+     * @param dimension The base physical dimensions of this unit.
      */
-    constructor(public readonly latex: string) { }
-}
-/**
- * Contains information on how units should be converted.
- */
-export class Simple extends Conversion {
+    constructor(public readonly latex: string, public readonly scale: number = 1, public readonly dimension: Dimension = new Dimension({})) { }
     /**
-     * The base physical dimensions of this simple unit.
-     */
-    public readonly dim: Dimension;
-    /**
-     * Create a new unit conversion for simple units.
+     * Create a simple unit conversion that defines this unit as the base for its dimension.
      * @param latex The LaTeX representation of this unit.
      * @param exponents The exponents on the base physical dimensions.
+     * @returns A simple conversion formula.
      */
-    constructor(latex: string, exponents: DimensionExponents) {
-        super(latex);
-        this.dim = new Dimension(exponents);
+    public static simple(latex: string, exponents: DimensionExponents): Conversion {
+        return new Conversion(latex, 1, new Dimension(exponents));
     }
-}
-export class Scaled extends Conversion {
-    /**
-     * The scale of this unit in relation to the base unit for this dimension.
-     */
-    public readonly scale: number;
-    /**
-     * Stores the unscaled base unit for this dimension.
-     */
-    public readonly baseUnits: Unit;
     /**
      * Create a new scaled unit using a metric prefix.
-     * @param prefix The metric prefix of this unit.
-     * @param base The base unit of this dimension.
+     * @param prefix The metric prefix of this scaled unit.
+     * @param base The unscaled base unit of this dimension.
+     * @returns A scaling conversion formula.
      */
-    constructor(prefix: Prefix.Name, base: Units) {
-        const unitTemp: Unit = new Unit({ [base]: 1 });
-        super(Prefix.Table[prefix].latex + ' ' + unitTemp.toString());
-        this.scale = Prefix.Table[prefix].scale;
-        this.baseUnits = unitTemp;
+    public static scaled(prefix: Prefix.Name, base: Units): Conversion {
+        return new Conversion(Prefix.Table[prefix].latex + ' ' + ConversionTable[base].latex, Prefix.Table[prefix].scale * ConversionTable[base].scale, ConversionTable[base].dimension);
     }
-}
-/**
- * Contains information on how units should be converted.
- */
-export class Complex extends Conversion {
     /**
-     * Stores the base units for a unit combined from other units. For example, `kg*m/s^2` for Newtons.
-     */
-    public readonly baseUnits: Unit;
-    /**
-     * Create a new unit conversion for complex units.
+     * Create a new unit that is relative to another.
      * @param latex The LaTeX representation of this unit.
-     * @param scale The scale of this unit in relation to the base units that make up this dimension.
-     * @param makeup The equivalent makeup of base units.
+     * @param scale The scale of this unit in relation to the relative unit.
+     * @param to The relative unit.
+     * @returns A relative scale formula between this unit and a relative one.
      */
-    constructor(latex: string, public readonly scale: number, makeup: UnitExponents) {
-        super(latex);
-        this.baseUnits = new Unit(makeup);
+    public static relative(latex: string, scale: number, to: Units): Conversion {
+        return new Conversion(latex, scale * ConversionTable[to].scale, ConversionTable[to].dimension);
+    }
+    /**
+     * @param latex The LaTeX representation of this unit.
+     * @param makeup The equivalent makeup of base units for a unit combined from other units. For example, `kg*m/s^2` for Newtons.
+     */
+    public static complex(latex: string, makeup: UnitExponents): Conversion {
+        let scale: number = 1,
+            dim: Dimension = new Dimension({});
+        for (const unit in makeup) {
+            const exponent: number = makeup[unit as Units] ?? 0;
+            scale *= (ConversionTable[unit as Units].scale ** exponent);
+            dim = dim.mult(ConversionTable[unit as Units].dimension, exponent);
+        }
+        return new Conversion(latex, scale, dim);
     }
 }
 /**
  * Defines the type for the conversion table.
  */
-type Conversions<T extends Conversion> = { [index in Units]: T };
+type Conversions = { [index in Units]: Conversion };
 /**
  * Represents the full conversion table for **absolute** units only.
  */
-export const ConversionTable: Conversions<Conversion> = {
+export const ConversionTable: Conversions = {
     nanosecond: new Scaled('nano', 'second'),
     millisecond: new Complex('\\text{ms}', 1e-3, { second: 1 }),
     second: new Simple('\\text{s}', { time: 1 }),
