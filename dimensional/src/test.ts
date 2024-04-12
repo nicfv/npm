489267fae495c3d6eab79d5a0eac7e7133562136
nicfv <envneo+github@gmail.com>
2024-04-11 22:51:00 -0700

    A few more tests for good measure

--- dimensional/src/test.ts
+++ dimensional/src/test.ts
@@ -19,6 +19,8 @@ import { U, Unit } from './unit';
         miles_per_hour: Unit = U({ mile: 1, hour: -1 });
     X.true(meters_per_second.dimension.is(miles_per_hour.dimension));
     X.false(meters_per_second.is(miles_per_hour));
+    X.gt(miles_per_hour.scale, 0.44); // 0.447...
+    X.lt(miles_per_hour.scale, 0.45);
     X.eq(meters_per_second.getNonzeroExponents().length, 2);
     X.eq(meters_per_second.getExponent('meter'), 1);
     X.eq(meters_per_second.getExponent('day'), 0);
@@ -27,7 +29,8 @@ import { U, Unit } from './unit';
 
 {
     const kN: Unit = U({ kiloNewton: 1 }),
-        kg_m_s2: Unit = U({ kilogram: 1, meter: 1, second: -2 });
+        kg_m_s2: Unit = U({ kilogram: 1, meter: 1, second: -2 }),
+        lbf: Unit = U({ pound_force: 1 });
     X.true(kN.dimension.is(kg_m_s2.dimension));
     X.false(kN.is(kg_m_s2));
     X.eq(kN.getNonzeroExponents().length, 1);
@@ -35,4 +38,8 @@ import { U, Unit } from './unit';
     X.eq(kN.getExponent('Newton'), 0);
     X.eq(kN.scale, 1000);
     X.is(kN.toString(), '\\text{kN}');
+    X.true(kN.dimension.is(lbf.dimension));
+    X.gt(lbf.scale, 4.44);// 4.448...
+    X.lt(lbf.scale, 4.45);
+    X.is(lbf.dimension.toString(), '\\frac{\\textbf{M} \\cdot \\textbf{L}}{\\textbf{T}^{2}}')
 }
