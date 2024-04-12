1771334030142b13f550b94730ac37493c58e614
nicfv <envneo+github@gmail.com>
2024-04-12 15:29:12 -0700

    tsdoc, add is() to check dimensions

--- dimensional/src/quantity.ts
+++ dimensional/src/quantity.ts
@@ -1,18 +1,54 @@
 import { SMath } from 'smath';
 import { Unit } from './unit';
-
+import { Dimension } from './dimension';
+/**
+ * Contains all software used for the calculation of physical quantities.
+ */
 export namespace Quantity {
+    /**
+     * Defines the class for physical quantities.
+     */
     export class Quantity {
+        /**
+         * The value converted to base units
+         */
         private readonly base: number;
+        /**
+         * Create a new instance of a quantity.
+         * @param value The value of the quantity
+         * @param unit The units attached to the value
+         */
         constructor(private readonly value: number, private readonly unit: Unit.Unit) {
             this.base = value;
             this.base = SMath.expand(value, 0, unit.scale);
         }
+        /**
+         * Convert this quantity to another set of units.
+         * Will throw an error if dimensions do not match.
+         * @param newUnit The unit to convert to
+         * @returns An equivalent quantity with new units
+         */
         public as(newUnit: Unit.Unit): Quantity {
-            if (!this.unit.dimension.is(newUnit.dimension)) {
+            if (!this.is(newUnit)) {
                 throw new Error('\\text{Dimensions do not match! } ' + this.unit.dimension.toString() + ' \\text{ vs. } ' + newUnit.dimension.toString())
             }
             return new Quantity(SMath.normalize(this.base, 0, newUnit.scale), newUnit);
+        }
+        /**
+         * Determine whether the dimensions of this quantity matches the input object.
+         * @param other Another quantity, unit, or dimension
+         * @returns True if the dimensions match, false otherwise
+         */
+        public is(other: Quantity | Unit.Unit | Dimension.Dimension): boolean {
+            if (other instanceof Quantity) {
+                return this.unit.dimension.is(other.unit.dimension);
+            } else if (other instanceof Unit.Unit) {
+                return this.unit.dimension.is(other.dimension);
+            } else if (other instanceof Dimension.Dimension) {
+                return this.unit.dimension.is(other);
+            } else {
+                throw new Error('Cannot compare to ' + (typeof other) + '!');
+            }
         }
         /**
          * Multiply this quantity by a constant scalar factor and return the result.
