c7572da754a0edd482c22e703a00515b79259143
nicfv <envneo+github@gmail.com>
2024-04-13 14:55:21 -0700

    Add M() and export

--- dimensional/src/lib.ts
+++ dimensional/src/lib.ts
@@ -1,4 +1,5 @@
 import { Dimension } from './dimension';
+import { Measure } from './measure';
 import { Quantity } from './quantity';
 import { Unit } from './unit';
 
@@ -9,6 +10,21 @@ import { Unit } from './unit';
  */
 export function D(exponents: Dimension.Exponents): Dimension.Dimension {
     return new Dimension.Dimension(exponents);
+}
+
+/**
+ * Shorthand for creating a new measurement type.
+ * @param name_or_exponents The raw name or exponents that make up this measurement
+ * @returns A new measurement object
+ */
+export function M(name_or_exponents: Measure.Name | Measure.Exponents): Measure.Measure {
+    if (typeof name_or_exponents === 'object') {
+        return new Measure.Measure('', name_or_exponents);
+    } else if (typeof name_or_exponents === 'string') {
+        return Measure.get(name_or_exponents);
+    } else {
+        throw new Error('Cannot create a new measurement type from ' + (typeof name_or_exponents) + '!');
+    }
 }
 
 /**
