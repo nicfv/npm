f67e8a73f260f820959ec72e291261ed6c5a4429
nicfv <envneo+github@gmail.com>
2024-04-15 09:14:08 -0700

    Reorder funcs in lib

--- dimensional/src/lib.ts
+++ dimensional/src/lib.ts
@@ -4,21 +4,13 @@ import { Quantity } from './quantity';
 import { Unit } from './unit';
 
 /**
- * Shorthand for creating a set of physical base dimensions.
- * @param exponents Exponents of each of the dimensions
- * @returns A new dimension object
+ * Shorthand for creating a new measured quantity.
+ * @param value The numerical value of the measurement
+ * @param unit The physical units of the measurement
+ * @returns A new quantity object
  */
-export function D(exponents: Dimension.Exponents | Dimension.Name): Dimension.Dimension {
-    return new Dimension.Dimension((typeof exponents === 'object') ? exponents : { [exponents]: 1 });
-}
-
-/**
- * Shorthand for creating a new attribute.
- * @param exponents Exponents that make up this attribute
- * @returns A new attribute object
- */
-export function A(exponents: Attribute.Exponents | Attribute.Name): Attribute.Attribute {
-    return new Attribute.Attribute((typeof exponents === 'object') ? exponents : { [exponents]: 1 });
+export function Q(value: number, unit: Unit.Unit): Quantity.Quantity {
+    return new Quantity.Quantity(value, unit);
 }
 
 /**
@@ -31,11 +23,19 @@ export function U(exponents: Unit.Exponents | Unit.Name): Unit.Unit {
 }
 
 /**
- * Shorthand for creating a new measured quantity.
- * @param value The numerical value of the measurement
- * @param unit The physical units of the measurement
- * @returns A new quantity object
+ * Shorthand for creating a new attribute.
+ * @param exponents Exponents that make up this attribute
+ * @returns A new attribute object
  */
-export function Q(value: number, unit: Unit.Unit): Quantity.Quantity {
-    return new Quantity.Quantity(value, unit);
+export function A(exponents: Attribute.Exponents | Attribute.Name): Attribute.Attribute {
+    return new Attribute.Attribute((typeof exponents === 'object') ? exponents : { [exponents]: 1 });
+}
+
+/**
+ * Shorthand for creating a set of physical base dimensions.
+ * @param exponents Exponents of each of the dimensions
+ * @returns A new dimension object
+ */
+export function D(exponents: Dimension.Exponents | Dimension.Name): Dimension.Dimension {
+    return new Dimension.Dimension((typeof exponents === 'object') ? exponents : { [exponents]: 1 });
 }
