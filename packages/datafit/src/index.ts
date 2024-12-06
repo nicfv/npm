ac098fdf624433985d01b3ecf7bb024640fce62d
nicfv <github@nicfv.com>
2024-12-05 21:26:40 -0800

    Badge on new line

--- packages/datafit/src/index.ts
+++ packages/datafit/src/index.ts
@@ -1,6 +1,8 @@
 /**
  * @packageDocumentation
- * ![NPM Downloads](https://img.shields.io/npm/d18m/datafit) Simple curve-fitting algorithm
+ * Simple curve-fitting algorithm
+ * 
+ * ![NPM Downloads](https://img.shields.io/npm/d18m/datafit)
  */
 export * from './types';
 export * from './lib';
