cd8c8894f9ca7f8d7d23f6128984ba2555d7d379
Nic <github@nicfv.com>
2025-02-02 14:03:02 -0800

    Add some functionality

--- packages/buildform/src/TextInput.ts
+++ packages/buildform/src/TextInput.ts
@@ -8,11 +8,13 @@ export interface TextOptions {
 }
 
 export class TextInput extends FormInput {
+    private readonly inputHandle: HTMLInputElement;
     private value: string = '';
     constructor(options: TextOptions) {
         const input: HTMLInputElement = document.createElement('input'),
             label: HTMLLabelElement = document.createElement('label');
         super([input, label]);
+        this.inputHandle = input;
         input.setAttribute('id', this.getId());
         label.setAttribute('for', this.getId());
         input.setAttribute('title', options.title ?? '');
@@ -21,6 +23,18 @@ export class TextInput extends FormInput {
         label.setAttribute('placeholder', options.placeholder ?? '');
         label.setAttribute('maxlength', (options.maxLength ?? 0).toFixed());
         input.addEventListener('input', () => this.value = input.value);
+    }
+
+    public setReadonly(readonly: boolean): void {
+        if (readonly) {
+            this.inputHandle.setAttribute('readonly', 'true');
+        } else {
+            this.inputHandle.removeAttribute('readonly');
+        }
+    }
+
+    public setValue(value: string): void {
+        this.inputHandle.value = value;
     }
 
     public getValue(): string {
