48473f209acd9e094f5ec8e1b6616c1e852c89a2
nicfv <github@nicfv.com>
2025-02-08 13:14:58 -0800

    Export more and show last update badge

--- packages/psychart/src/index.ts
+++ packages/psychart/src/index.ts
@@ -3,5 +3,8 @@
  * View air conditions on a psychrometric chart
  * 
  * ![NPM Downloads](https://img.shields.io/npm/d18m/psychart)
+ * ![NPM Last Update](https://img.shields.io/npm/last-update/psychart)
  */
 export * from './psychart';
+export * from './psystate';
+export * from './types';
