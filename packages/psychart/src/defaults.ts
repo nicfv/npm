555859b75f9d5b661fc0bc8d7e0dd44a53a50114
Nic <github@nicfv.com>
2025-03-09 10:07:16 -0700

    Create setDefaults function

--- packages/psychart/src/defaults.ts
+++ packages/psychart/src/defaults.ts
@@ -1,6 +1,20 @@
 import { Color } from 'viridis';
 import { PsychartOptions, DataOptions } from './types';
 
+/**
+ * Take an object with all optional values, and set all unset values to their defaults.
+ * @param optional An object with all parameters optional.
+ * @param defaults An object with all parameters default.
+ * @returns An object with all parameters that are unset as their default values.
+ */
+export function setDefaults<T extends { [key: string]: any }>(optional: Partial<T>, defaults: T): T {
+    const required: T = defaults;
+    for (const key in required) {
+        required[key] = optional[key] ?? defaults[key];
+    }
+    return required;
+}
+
 export function getDefaultPsychartOptions(isDarkTheme: boolean): PsychartOptions {
     return {
         altitude: 0,
