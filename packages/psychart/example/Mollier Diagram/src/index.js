aa0bf882f3578acbcce86f103b7a96d1cd05ff0e
Nic <github@nicfv.com>
2025-03-09 21:33:37 -0700

    Fix one of the examples

--- packages/psychart/example/Mollier Diagram/src/index.js
+++ packages/psychart/example/Mollier Diagram/src/index.js
@@ -1,14 +1,13 @@
-import { getDefaultPsychartOptions, Psychart } from 'psychart';
+import { Psychart } from 'psychart';
 
-const customPsyOptions = getDefaultPsychartOptions(false);
-customPsyOptions.unitSystem = 'SI';
-customPsyOptions.dbMax = 50; // [deg C]
-customPsyOptions.dbMin = 10; // [deg C]
-customPsyOptions.dpMax = 40; // [deg C]
-customPsyOptions.flipXY = true;
-customPsyOptions.yAxis = 'hr'; // Humidity Ratio
-
-const ps = new Psychart(customPsyOptions);
+const ps = new Psychart({
+    unitSystem: 'SI',
+    dbMax: 50,// [deg C]
+    dbMin: 10,// [deg C]
+    dpMax: 40,// [deg C]
+    flipXY: true,
+    yAxis: 'hr', // Humidity Ratio
+});
 
 window.addEventListener('load', () => {
     document.body.append(ps.getElement());
