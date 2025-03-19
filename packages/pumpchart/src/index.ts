5f9bfa7fff750fe5c82244e24b7929231ea33f9a
nicfv <github@nicfv.com>
2025-03-19 11:08:15 -0700

    Add badges and default export

--- packages/pumpchart/src/index.ts
+++ packages/pumpchart/src/index.ts
@@ -1,4 +1,8 @@
 /**
  * @packageDocumentation
  * Show a pump's relationship between flow rate and pressure at different operating points
+ * 
+ * ![NPM Downloads](https://img.shields.io/npm/d18m/pumpchart)
+ * ![NPM Last Update](https://img.shields.io/npm/last-update/pumpchart)
  */
+export const TEST: string = 'It works!';
