1116b4cc291bcacbf6fbf6bb04e220c6ca29f821
Nic <github@nicfv.com>
2025-03-19 22:18:59 -0700

    Style fix

--- packages/psychart/example/Mollier Diagram/src/index.js
+++ packages/psychart/example/Mollier Diagram/src/index.js
@@ -2,9 +2,9 @@ import { Psychart } from 'psychart';
 
 const ps = new Psychart({
     unitSystem: 'SI',
-    dbMax: 50,// [deg C]
-    dbMin: 10,// [deg C]
-    dpMax: 40,// [deg C]
+    dbMax: 50, // [deg C]
+    dbMin: 10, // [deg C]
+    dpMax: 40, // [deg C]
     flipXY: true,
     yAxis: 'hr', // Humidity Ratio
 });
