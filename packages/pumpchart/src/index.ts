11dd532b57650d50941ec100ec8d1618dd599c01
Nic <github@nicfv.com>
2025-07-06 21:02:25 -0700

    Delete unused packages

--- packages/pumpchart/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @packageDocumentation
- * Show a pump's relationship between flow rate and pressure at different operating points
- * 
- * ![NPM Downloads](https://img.shields.io/npm/d18m/pumpchart)
- * ![NPM Last Update](https://img.shields.io/npm/last-update/pumpchart)
- */
-export const TEST: string = 'It works!';
