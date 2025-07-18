1ece5f039fc1632f96260906a68b1afc4b3e0bdf
nicfv <github@nicfv.com>
2025-07-18 15:05:18 -0700

    Create base class for dimensions

---
+++ packages/dimensional/src/Dimension.ts
@@ -0,0 +1,46 @@
+/**
+ * Represents a physical base dimension.
+ */
+export class Dimension {
+    /**
+     * Contains a dictionary of all created dimensions.
+     */
+    private static readonly dictionary: { [name: string]: Dimension } = {};
+    /**
+     * Return a list of valid dimension plain text names.
+     * @returns A list of names for all dimensions
+     */
+    public static getNames(): Array<string> {
+        return Object.keys(this.dictionary);
+    }
+    /**
+     * Return a dimension with the matching plain text name, if one exists.
+     * @param name A plain text name to match
+     * @returns The matching `Dimension`
+     */
+    public static get(name: string): Dimension | undefined {
+        return this.dictionary[name];
+    }
+    /**
+     * Create a new base dimension.
+     * @param name The unique name for this `Dimension`
+     * @param symbol The mathematic symbol for this `Dimension`
+     */
+    constructor(private readonly name: string, private readonly symbol: string) {
+        if (!Dimension.dictionary[name]) {
+            Dimension.dictionary[name] = this;
+        }
+    }
+    /**
+     * Generate and return a string representation of this dimension.
+     * @param type The type of string to return
+     * @returns A string representation of this `Dimension`
+     */
+    public toString(type: 'plain' | 'LaTeX' = 'plain'): string {
+        if (type === 'plain') {
+            return this.name;
+        } else {
+            return '\\textbf{' + this.symbol + '}';
+        }
+    }
+}
