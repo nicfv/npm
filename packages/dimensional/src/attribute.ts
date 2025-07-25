dcd1d525e419a1ca5d9bd9f8f6de2fe1b2eca30e
nicfv <github@nicfv.com>
2025-07-25 14:09:47 -0700

    Attributes now calculate base dims

--- packages/dimensional/src/attribute.ts
+++ packages/dimensional/src/attribute.ts
@@ -1,129 +1,28 @@
+import { Compound, MathSymbol } from './compound';
 import { Dimension } from './dimension';
-import { factor } from './lib';
-import { AlreadyExistsError, Factors, NotFoundError } from './types';
 
-export class Attribute {
+/**
+ * Represents a type of measured quantity.
+ */
+export class Attribute implements MathSymbol {
     /**
-     * Contains a dictionary of all created attributes.
+     * Contains a compound of the physical base dimensions for this Attribute.
      */
-    private static readonly dictionary: { [name: string]: Attribute } = {};
+    public readonly baseDimensions: Compound<Dimension>;
     /**
-     * Return a list of valid atribute plain text names.
-     * @returns A list of names for all attributes
+     * Define a new attribute.
+     * @param LaTeX The LaTeX code for this `Attribute`
+     * @param baseDimensionOrAttributes The physical base dimension, or makeup of attributes
      */
-    public static getNames(): Array<string> {
-        return Object.keys(this.dictionary);
-    }
-    /**
-     * Return an attribute with the matching plain text name, if one exists.
-     * @param name A plain text name to match
-     * @returns The matching `Attribute`
-     */
-    public static get(name: string): Attribute | undefined {
-        return this.dictionary[name];
-    }
-    /**
-     * The base physical dimensions for this attribute.
-     */
-    private readonly baseDimensions: Factors = {};
-    /**
-     * Numerator
-     */
-    private readonly num: Factors = {};
-    /**
-     * Denominator
-     */
-    private readonly den: Factors = {};
-    /**
-     * Create a new attribute.
-     * @param name The unique name for this attribute
-     * @param symbol The mathematical symbol for this attribute
-     * @param baseDimensionOrAttributeFactors Either the base dimension name as a string, or the makeup of attribute factors and their exponents
-     */
-    constructor(private readonly name: string, private readonly symbol: string, baseDimensionOrAttributeFactors: string | Factors) {
-        if (Attribute.get(name)) {
-            throw new AlreadyExistsError('Attribute', name);
+    constructor(public readonly LaTeX: string, baseDimensionOrAttributes: Dimension | Compound<Attribute>) {
+        if (baseDimensionOrAttributes instanceof Dimension) {
+            this.baseDimensions = new Compound(baseDimensionOrAttributes);
         } else {
-            Attribute.dictionary[name] = this;
-        }
-        if (typeof baseDimensionOrAttributeFactors === 'string') {
-            const baseDimension = Dimension.get(baseDimensionOrAttributeFactors);
-            if (baseDimension) {
-                this.baseDimensions[baseDimension.toString('plain')] = 1;
-            } else {
-                throw new NotFoundError('Dimension', baseDimensionOrAttributeFactors);
-            }
-        } else {
-            for (const attributeName in baseDimensionOrAttributeFactors) {
-                if (name === attributeName) {
-                    throw new Error('Recursive loop detected in attribute factors.');
-                }
-                const attribute = Attribute.get(attributeName);
-                if (!attribute) {
-                    throw new NotFoundError('Attribute', attributeName);
-                }
-                const attributeExponent: number = baseDimensionOrAttributeFactors[attributeName];
-                for (const dimensionName in attribute.baseDimensions) {
-                    const dimensionExponent: number = attribute.baseDimensions[dimensionName];
-                    this.baseDimensions[dimensionName] = (this.baseDimensions[dimensionName] ?? 0) + attributeExponent * dimensionExponent;
-                }
+            this.baseDimensions = new Compound();
+            for (const attribute of baseDimensionOrAttributes.getTerms()) {
+                const exponent: number = baseDimensionOrAttributes.getExponent(attribute);
+                this.baseDimensions = this.baseDimensions.times(attribute.baseDimensions.pow(exponent));
             }
         }
-        for (const dimensionName in this.baseDimensions) {
-            const dimension = Dimension.get(dimensionName);
-            if (!dimension) {
-                throw new NotFoundError('Dimension', dimensionName);
-            }
-            const exponent: number = this.baseDimensions[dimensionName];
-            if (exponent > 0) {
-                this.num[dimensionName] = exponent;
-            } else if (exponent < 0) {
-                this.den[dimensionName] = -exponent;
-            }
-        }
-    }
-    public toString(type: 'plain' | 'symbol' | 'base-dimensions' = 'plain'): string {
-        switch (type) {
-            case ('plain'): {
-                return this.name;
-            }
-            case ('symbol'): {
-                return this.symbol;
-            }
-            case ('base-dimensions'): {
-                let numerator: string = '',
-                    denominator: string = '';
-                for (const dimensionName in this.num) {
-                    const dimension = Dimension.get(dimensionName),
-                        exponent: number = this.num[dimensionName];
-                    if (!dimension) {
-                        throw new NotFoundError('Dimension', dimensionName);
-                    }
-                    if (numerator) {
-                        numerator += '\\cdot';
-                    }
-                    numerator += factor(dimension.toString('symbol'), exponent);
-                }
-                for (const dimensionName in this.den) {
-                    const dimension = Dimension.get(dimensionName),
-                        exponent: number = this.den[dimensionName];
-                    if (!dimension) {
-                        throw new NotFoundError('Dimension', dimensionName);
-                    }
-                    if (denominator) {
-                        denominator += '\\cdot';
-                    }
-                    denominator += factor(dimension.toString('symbol'), exponent);
-                }
-                if (denominator) {
-                    return '\\frac{' + (numerator || '1') + '}{' + denominator + '}';
-                } else {
-                    return numerator || '1';
-                }
-            }
-        }
-    }
-    public getBaseDimensions(): Factors {
-        return JSON.parse(JSON.stringify(this.baseDimensions));
     }
 }
--- packages/dimensional/src/test.ts
+++ packages/dimensional/src/test.ts
@@ -1,9 +1,19 @@
 import * as T6 from 't6';
-// import { Dimension } from './dimension';
-// import { Attribute } from './attribute';
-import { Compound, MathSymbol } from './compound';
+import { Dimension } from './dimension';
+import { Attribute } from './attribute';
+import { Compound } from './compound';
 
 T6.isTrue(true);
+
+const length = new Dimension('L'),
+    time = new Dimension('T');
+
+const duration = new Attribute('t', time),
+    distance = new Attribute('x', length),
+    velocity = new Attribute('v', new Compound([[distance, 1], [duration, -1]])),
+    acceleration = new Attribute('a', new Compound([[velocity, 1], [duration, -1]]));
+
+console.log(acceleration.LaTeX, acceleration.baseDimensions.toString());
 
 // new Dimension('mass', 'M');
 // new Dimension('length', 'L');
@@ -18,19 +28,20 @@ T6.isTrue(true);
 // console.log(Attribute.get('acceleration')?.getBaseDimensions());
 // console.log(Attribute.get('acceleration')?.toString('base-dimensions'));
 
-type hiscore = { name: string, score: number } & MathSymbol;
+// type hiscore = { name: string, score: number } & MathSymbol;
 
-const hiscore1: hiscore = { name: 'asdf', score: 12, LaTeX: 'A' },
-    hiscore2: hiscore = { name: 'fdsa', score: 21, LaTeX: 'B' };
+// const hiscore1: hiscore = { name: 'hs1', score: 32, LaTeX: 'A' },
+//     hiscore2: hiscore = { name: 'hs2', score: 21, LaTeX: 'B' };
 
-const c = new Compound<hiscore>([
-    [hiscore1, 2],
-    [hiscore2, 3],
-    [hiscore1, 1],
-]),
-    d = new Compound<hiscore>([
-        [hiscore1, 4],
-        [hiscore2, -1],
-    ]);
-console.log(c.toString(), d.toString(), (c.over(d)).toString(), c.is(c), c.is(d));
-console.log(d.times(hiscore2).toString());
+// const c = new Compound<hiscore>([
+//     [hiscore1, 2],
+//     [hiscore2, 3],
+//     [hiscore1, 1],
+// ]),
+//     d = new Compound<hiscore>([
+//         [hiscore1, 4],
+//         [hiscore2, 1],
+//         [hiscore2, -1],
+//     ]);
+// console.log(c.toString(), d.toString(), (c.over(d)).toString(), c.is(c), c.is(d));
+// console.log(d.times(hiscore2).toString());
