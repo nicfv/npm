4a7c00526268f11e4568fee0f035dec5559d022b
nicfv <github@nicfv.com>
2025-07-25 13:59:07 -0700

    Major cleanup in the dimension class

--- packages/dimensional/src/dimension.ts
+++ packages/dimensional/src/dimension.ts
@@ -1,50 +1,12 @@
-import { AlreadyExistsError } from './types';
+import { MathSymbol } from './compound';
 
 /**
  * Represents a physical base dimension.
  */
-export class Dimension {
+export class Dimension implements MathSymbol {
     /**
-     * Contains a dictionary of all created dimensions.
+     * Define a new physical base dimension.
+     * @param LaTeX The mathematical symbol for this dimension
      */
-    private static readonly dictionary: { [name: string]: Dimension } = {};
-    /**
-     * Return a list of valid dimension plain text names.
-     * @returns A list of names for all dimensions
-     */
-    public static getNames(): Array<string> {
-        return Object.keys(this.dictionary);
-    }
-    /**
-     * Return a dimension with the matching plain text name, if one exists.
-     * @param name A plain text name to match
-     * @returns The matching `Dimension`
-     */
-    public static get(name: string): Dimension | undefined {
-        return this.dictionary[name];
-    }
-    /**
-     * Create a new base dimension.
-     * @param name The unique name for this `Dimension`
-     * @param symbol The mathematic symbol for this `Dimension`
-     */
-    constructor(private readonly name: string, private readonly symbol: string) {
-        if (Dimension.get(name)) {
-            throw new AlreadyExistsError('Dimension', name);
-        } else {
-            Dimension.dictionary[name] = this;
-        }
-    }
-    /**
-     * Generate and return a string representation of this dimension.
-     * @param type The type of string to return
-     * @returns A string representation of this `Dimension`
-     */
-    public toString(type: 'plain' | 'symbol' = 'plain'): string {
-        if (type === 'plain') {
-            return this.name;
-        } else {
-            return '\\textbf{' + this.symbol + '}';
-        }
-    }
+    constructor(public readonly LaTeX: string) { }
 }
