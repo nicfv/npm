bfd851d77676b57eab6144b7a5af7ff2a97d9511
Nic <github@nicfv.com>
2025-07-25 17:49:48 -0700

    Just put it in the static class instead

--- packages/dimensional/src/dimension.ts
+++ packages/dimensional/src/dimension.ts
@@ -4,22 +4,16 @@ import { MathSymbol } from './compound';
  * Represents a physical base dimension.
  */
 export class Dimension implements MathSymbol {
+    public static readonly Mass = new Dimension('\\textbf{M}');
+    public static readonly Length = new Dimension('\\textbf{L}');
+    public static readonly Time = new Dimension('\\textbf{T}');
+    public static readonly Temperature = new Dimension('\\Theta');
+    public static readonly Current = new Dimension('\\textbf{I}');
+    public static readonly LuminousIntensity = new Dimension('\\textbf{J}');
+    public static readonly AmountOfSubstance = new Dimension('\\textbf{N}');
     /**
      * Define a new physical base dimension.
      * @param LaTeX The mathematical symbol for this dimension
      */
     constructor(public readonly LaTeX: string) { }
 }
-
-/**
- * Writable dictionary containing base dimensions
- */
-export const Dimensions: { [dimension: string]: Dimension } = {
-    Mass: new Dimension('\\textbf{M}'),
-    Length: new Dimension('\\textbf{L}'),
-    Time: new Dimension('\\textbf{T}'),
-    Temperature: new Dimension('\\Theta'),
-    Current: new Dimension('\\textbf{I}'),
-    LuminousIntensity: new Dimension('\\textbf{J}'),
-    AmountOfSubstance: new Dimension('\\textbf{N}'),
-};
