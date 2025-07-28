cef9efe72b6b39086141047d7c9008474980d3fa
Nicolas Ventura <ventura@lbl.gov>
2025-07-28 14:39:37 -0700

    Bring back prefixes, but make it a class (so you can create custom prefixes

---
+++ packages/dimensional/src/prefix.ts
@@ -0,0 +1,11 @@
+/**
+ * Represents a unit prefix, which is effectively a unit modifier by a scaling factor.
+ */
+export class Prefix {
+    /**
+     * Define a new prefix to be used for any unit.
+     * @param LaTeX The LaTeX code for this prefix
+     * @param scale The scale of this prefix, relative to the base unit
+     */
+    constructor(public readonly LaTeX: string, public readonly scale: number) { }
+}
