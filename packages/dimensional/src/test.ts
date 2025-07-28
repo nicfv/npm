4a9678c0fb7771258882537d1a30b209e34df7e5
Nicolas Ventura <ventura@lbl.gov>
2025-07-28 13:51:41 -0700

    Remove attribute, clean up unit (no attribute, straight to dimension

--- packages/dimensional/src/test.ts
+++ packages/dimensional/src/test.ts
@@ -1,59 +1,14 @@
 import * as T6 from 't6';
-import { Attribute } from './attribute';
 import { Unit } from './unit';
 import { Compound } from './compound';
+import { Dimension } from './dimension';
 
 T6.isTrue(true);
 
-console.log(Attribute.Acceleration.LaTeX, Attribute.Acceleration.baseDimensions.toString());
-
-// const force = new Attribute('F', new Compound<Attribute>([[Attribute.Mass, 1], [Attribute.Acceleration, 1]]));
-
-const meter = new Unit('m', { base: Attribute.Length }),
-    second = new Unit('s', { base: Attribute.Time }),
-    gram = new Unit('g', { base: Attribute.Mass }),
-    kilogram = new Unit('kg', { unit: gram, scale: 1e3 }),
-    // centimeter = new Unit('cm', { units: new Compound<Unit>(meter), scale: 1e-2 }),
+const meter = new Unit('m', { base: Dimension.Length }),
+    second = new Unit('s', { base: Dimension.Time }),
+    gram = new Unit('g', { base: Dimension.Mass }),
+    kilogram = new Unit('kg', { unit: gram, scale: 1000 }),
     newton = new Unit('N', { units: new Compound([[kilogram, 1], [meter, 1], [second, -2]]) });
-console.log(newton.LaTeX, newton.baseAttributes.toString(), newton.baseDimensions.toString(), newton.scale);
 
-// const length = new Dimension('L'),
-//     time = new Dimension('T');
-
-// const duration = new Attribute('t', time),
-//     distance = new Attribute('x', length),
-//     velocity = new Attribute('v', new Compound([[distance, 1], [duration, -1]])),
-//     acceleration = new Attribute('a', new Compound([[velocity, 1], [duration, -1]]));
-
-// console.log(acceleration.LaTeX, acceleration.baseDimensions.toString());
-
-// new Dimension('mass', 'M');
-// new Dimension('length', 'L');
-// new Dimension('time', 'T');
-
-// new Attribute('length', 'x', 'length');
-// new Attribute('time', 't', 'time');
-// new Attribute('velocity', 'v', { 'length': 1, 'time': -1 });
-// new Attribute('acceleration', 'a', { 'velocity': 1, 'time': -1 });
-
-// console.log(Dimension.get('mass')?.toString(), Dimension.getNames());
-// console.log(Attribute.get('acceleration')?.getBaseDimensions());
-// console.log(Attribute.get('acceleration')?.toString('base-dimensions'));
-
-// type hiscore = { name: string, score: number } & MathSymbol;
-
-// const hiscore1: hiscore = { name: 'hs1', score: 32, LaTeX: 'A' },
-//     hiscore2: hiscore = { name: 'hs2', score: 21, LaTeX: 'B' };
-
-// const c = new Compound<hiscore>([
-//     [hiscore1, 2],
-//     [hiscore2, 3],
-//     [hiscore1, 1],
-// ]),
-//     d = new Compound<hiscore>([
-//         [hiscore1, 4],
-//         [hiscore2, 1],
-//         [hiscore2, -1],
-//     ]);
-// console.log(c.toString(), d.toString(), (c.over(d)).toString(), c.is(c), c.is(d));
-// console.log(d.times(hiscore2).toString());
+console.log(newton.LaTeX, newton.baseDimensions.toString(), newton.scale);
--- packages/dimensional/src/unit.ts
+++ packages/dimensional/src/unit.ts
@@ -1,26 +1,19 @@
-import { Attribute } from './attribute';
 import { Compound, MathSymbol } from './compound';
 import { Dimension } from './dimension';
 
 export class Unit implements MathSymbol {
     public readonly scale: number = 1;
-    public readonly baseAttributes: Compound<Attribute>;
     public readonly baseDimensions: Compound<Dimension>;
-    constructor(public readonly LaTeX: string, makeup: { base: Attribute } | { unit: Unit, scale: number } | { units: Compound<Unit> }) {
+    constructor(public readonly LaTeX: string, makeup: { base: Dimension } | { unit: Unit, scale: number } | { units: Compound<Unit> }) {
         if ('base' in makeup) {
-            this.baseAttributes = new Compound(makeup.base);
-            this.baseDimensions = new Compound(makeup.base.baseDimensions);
+            this.baseDimensions = new Compound(makeup.base);
         } else if ('unit' in makeup) {
             this.scale = makeup.scale;
-            console.log(LaTeX, this.scale, makeup.scale);
-            this.baseAttributes = new Compound(makeup.unit.baseAttributes);
             this.baseDimensions = new Compound(makeup.unit.baseDimensions);
         } else {
-            this.baseAttributes = new Compound();
             this.baseDimensions = new Compound();
             for (const unit of makeup.units.getTerms()) {
                 const exponent: number = makeup.units.getExponent(unit);
-                this.baseAttributes = this.baseAttributes.times(unit.baseAttributes.pow(exponent));
                 this.baseDimensions = this.baseDimensions.times(unit.baseDimensions.pow(exponent));
                 this.scale *= (unit.scale ** exponent);
             }
