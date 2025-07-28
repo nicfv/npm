73132311d06d897e5b0824c53af6feb9cf700612
Nicolas Ventura <ventura@lbl.gov>
2025-07-28 15:57:48 -0700

    unit conversion!!

--- packages/dimensional/src/quantity.ts
+++ packages/dimensional/src/quantity.ts
@@ -18,22 +18,17 @@ export class Quantity {
             this.scale = compoundUnit.scale;
         }
     }
+    public scaleBy(factor: number): Quantity {
+        return new Quantity(this.quantity * factor, this.units);
+    }
     public plus(other: Quantity): Quantity {
-        if (this.dimensions.is(other.dimensions)) {
-            return new Quantity(this.quantity + other.quantity * other.scale / this.scale, this.units);
-        } else {
-            throw new DimensionMismatch(this, other);
-        }
+        return new Quantity(this.quantity + other.as(this.units).quantity, this.units);
     }
     public minus(other: Quantity): Quantity {
-        return this.plus(new Quantity(-other.quantity, other.units));
+        return new Quantity(this.quantity - other.as(this.units).quantity, this.units);
     }
-    public times(other: Quantity | number): Quantity {
-        if (other instanceof Quantity) {
-            return new Quantity(this.quantity * other.quantity, this.units.times(other.units));
-        } else {
-            return new Quantity(this.quantity * other, this.units);
-        }
+    public times(other: Quantity): Quantity {
+        return new Quantity(this.quantity * other.quantity, this.units.times(other.units));
     }
     public over(other: Quantity): Quantity {
         return new Quantity(this.quantity / other.quantity, this.units.over(other.units));
@@ -41,13 +36,24 @@ export class Quantity {
     public pow(exponent: number): Quantity {
         return new Quantity(this.quantity ** exponent, this.units.pow(exponent));
     }
+    public as(units: Unit | Compound<Unit>): Quantity {
+        if (units instanceof Unit) {
+            units = new Compound(units);
+        }
+        if (this.dimensions.is(new Unit('', { units: units }).dimensions)) {
+            const compoundUnit: Unit = new Unit('', { units: units });
+            return new Quantity(this.quantity * compoundUnit.scale / this.scale, units);
+        } else {
+            throw new DimensionMismatch(this, units);
+        }
+    }
     public toString(): string {
         return this.quantity + ' \\left[ ' + this.units.toString() + ' \\right]';
     }
 }
 
 class DimensionMismatch extends Error {
-    constructor(me: Quantity, other: Quantity) {
-        super('Dimensions on ' + me.units.toString() + ' does not match dimensions on ' + other.units.toString());
+    constructor(me: Quantity, units: Compound<Unit>) {
+        super('Dimensions on ' + me.units.toString() + ' does not match dimensions on ' + units.toString());
     }
 }
