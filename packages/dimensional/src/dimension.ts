0565f1fdab059c2fe046a16a773a90902e38efae
Nicolas Ventura <ventura@lbl.gov>
2025-07-30 17:59:37 -0400

    Extend compound in Dimension

--- packages/dimensional/src/dimension.ts
+++ packages/dimensional/src/dimension.ts
@@ -1,12 +1,13 @@
-import { MathSymbol } from './compound';
+import { Compound } from './compound';
 
 /**
  * Represents a physical base dimension.
  */
-export class Dimension implements MathSymbol {
-    /**
-     * Define a new physical base dimension.
-     * @param LaTeX The mathematical symbol for this dimension
-     */
-    constructor(public readonly LaTeX: string) { }
+export class Dimension extends Compound<Dimension> {
+    public times(factor: Dimension): Dimension {
+        return new Dimension([...super.multiplyFactors(this, factor)]);
+    }
+    public pow(exponent: number): Dimension {
+        return new Dimension([...super.raiseFactors(this, exponent)]);
+    }
 }
