078f9db0ca49f9a1d726fcdc1c3c646b63627a6a
Nicolas Ventura <ventura@lbl.gov>
2025-07-30 18:03:02 -0400

    Revert "A test to allow overloading"
    This reverts commit d13d78fd29fcd1bac467f0ec29148cbcfe5afc52.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,4 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.strict, /*tseslint.configs.stylistic,*/ {
-    rules: {
-        '@typescript-eslint/unified-signatures': ['error', { 'ignoreDifferentlyNamedParameters': true }],
-    },
-});
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.strict);
