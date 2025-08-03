ffa7e1abd89c24b7310320d7eb6d33431e8b6432
Nicolas Ventura <ventura@lbl.gov>
2025-08-03 15:59:27 -0700

    final testing custom

--- packages/dimensional/src/test.ts
+++ packages/dimensional/src/test.ts
@@ -1,6 +1,8 @@
 import * as T6 from 't6';
 import { defaults } from './defaults';
 import { Dimension } from './dimension';
+import { Prefix } from './prefix';
+import { Unit } from './unit';
 
 {
     // Dimension
@@ -19,4 +21,46 @@ import { Dimension } from './dimension';
     T6.isFalse(customDimension.is(defaults.LuminousIntensity.pow(-2)));
     T6.isFalse(new Dimension('x').is(new Dimension('x'))); // These should be considered different dimensions
     T6.isTrue(defaults.Dimensionless.over(defaults.Length).is(defaults.Length.pow(-1)));
+}
+
+{
+    // Unit
+    // .toString
+    console.log(T6.getTestNumber());
+    T6.is(defaults.Kelvin.toString(), '\\text{K}');
+    T6.is(defaults.mile.toString(), '\\text{mi}');
+    T6.is(defaults.Rankine.toString(), '{^{\\circ}\\text{R}}');
+    T6.is(defaults.kilometer.toString(), '{\\text{k}\\text{m}}');
+    // .dimensions
+    T6.isTrue(defaults.Kelvin.dimensions.is(defaults.Temperature));
+    T6.isTrue(defaults.Newton.dimensions.is(defaults.force));
+    T6.isTrue(defaults.Newton.dimensions.is(defaults.Mass.times(defaults.acceleration)));
+    T6.isTrue(new Unit().dimensions.is(defaults.Dimensionless));
+    T6.isTrue(new Unit().is(defaults.Unitless));
+    T6.isTrue(new Unit('x').dimensions.is(defaults.Dimensionless)); // Unassigned base dimensions/units
+    // .prefix
+    const customKm = defaults.meter.prefix(defaults.kilo);
+    T6.is(customKm.toString(), defaults.kilometer.toString());
+    T6.isFalse(customKm.is(defaults.kilometer)); // Not seen as the same unit
+    let caught: boolean;
+    caught = false;
+    try {
+        customKm.prefix(defaults.milli);
+    } catch {
+        caught = true;
+    }
+    T6.isTrue(caught, 'Should not allow multiple prefixes.');
+}
+{
+    // Customization
+    const dimensionBlob = new Dimension('\\beta'),
+        customPrefix = new Prefix('\\textbf{p}_{5}', 5),
+        customInch = defaults.inch.prefix(customPrefix),
+        customUnit = customInch.over(new Unit('blob', dimensionBlob));
+    T6.is(dimensionBlob.toString(), '{\\beta}');
+    T6.is(customPrefix.LaTeX, '{\\textbf{p}_{5}}');
+    T6.is(customInch.toString(), '{{\\textbf{p}_{5}}\\text{in}}');
+    T6.is(customUnit.toString(), '\\frac{{{\\textbf{p}_{5}}\\text{in}}}{\\text{blob}}');
+    T6.is(customUnit.dimensions.toString(), '\\frac{{\\textbf{L}}}{{\\beta}}');
+    T6.isTrue(customUnit.dimensions.is(defaults.Length.over(dimensionBlob)));
 }
