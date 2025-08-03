c86d4428475ea28ac5437ec4e8dff667c834d6e3
Nicolas Ventura <ventura@lbl.gov>
2025-08-03 15:19:56 -0700

    Start writing tests

--- packages/dimensional/src/test.ts
+++ packages/dimensional/src/test.ts
@@ -1,11 +1,22 @@
 import * as T6 from 't6';
-import { Quantity } from './quantity';
 import { defaults } from './defaults';
+import { Dimension } from './dimension';
 
-T6.isTrue(true);
-
-const lb = new Quantity(130, defaults.pound),
-    accel = new Quantity(1, defaults.foot.over(defaults.second.pow(2)));
-
-console.log(lb.times(accel).as(defaults.Newton).toString())
-console.log(defaults.year.to(defaults.second));
+{
+    // Dimension
+    // .toString
+    T6.is(defaults.Dimensionless.toString(), '1');
+    T6.is(defaults.Mass.toString(), '{\\textbf{M}}');
+    T6.is(defaults.acceleration.toString(), '\\frac{{\\textbf{L}}}{{\\textbf{T}}^{2}}');
+    T6.is(defaults.Temperature.toString(), '{\\boldsymbol{\\Theta}}');
+    const customDimension = new Dimension('x').times(defaults.AmountOfSubstance).pow(2);
+    T6.is(customDimension.toString(), '\\text{x}^{2} \\cdot {\\textbf{N}}^{2}');
+    // .is
+    T6.isTrue(defaults.Dimensionless.is(new Dimension()));
+    T6.isTrue(defaults.velocity.is(defaults.Length.over(defaults.Time)));
+    T6.isTrue(defaults.force.is(defaults.Dimensionless.times(defaults.force)));
+    T6.isFalse(defaults.acceleration.is(defaults.Length.over(defaults.Time)));
+    T6.isFalse(customDimension.is(defaults.LuminousIntensity.pow(-2)));
+    T6.isFalse(new Dimension('x').is(new Dimension('x'))); // These should be considered different dimensions
+    T6.isTrue(defaults.Dimensionless.over(defaults.Length).is(defaults.Length.pow(-1)));
+}
