f05dae8d4fae534542f99afe828bde3c44b7c4c5
Nicolas Ventura <ventura@lbl.gov>
2025-08-14 15:54:11 -0700

    Create groups

--- packages/psychart/src/pumpchart/pumpchart.ts
+++ packages/psychart/src/pumpchart/pumpchart.ts
@@ -3,7 +3,11 @@ import { defaultPumpchartOptions } from './defaults';
 import { PumpchartOptions } from './types';
 
 export class Pumpchart extends Chart<PumpchartOptions> {
-    constructor(options: Partial<PumpchartOptions>) {
+    private readonly g = {
+        axes: document.createElementNS(this.NS, 'g'),
+    };
+    constructor(options: Partial<PumpchartOptions> = {}) {
         super(options, defaultPumpchartOptions);
+        Object.values(this.g).forEach(group => this.svg.appendChild(group));
     }
 }
