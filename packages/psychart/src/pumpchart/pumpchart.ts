e6abbf8c90a31d96d8f557df1685e378a5a49c4b
Nicolas Ventura <ventura@lbl.gov>
2025-08-14 16:54:36 -0700

    Render 2 axes

--- packages/psychart/src/pumpchart/pumpchart.ts
+++ packages/psychart/src/pumpchart/pumpchart.ts
@@ -1,8 +1,20 @@
+import * as SMath from 'smath';
 import { Chart } from '../chart';
 import { defaultPumpchartOptions } from './defaults';
-import { PumpchartOptions } from './types';
+import { Point, PumpchartOptions, State } from './types';
 
 export class Pumpchart extends Chart<PumpchartOptions> {
+    /**
+     * Represents (0 gpm, 0 ft)
+     */
+    private readonly origin: Point;
+    /**
+     * Represents (max gpm, max ft)
+     */
+    private readonly farCorner: Point;
+    /**
+     * Layers of the SVG as groups.
+     */
     private readonly g = {
         axes: document.createElementNS(this.NS, 'g'),
         curves: document.createElementNS(this.NS, 'g'),
@@ -10,11 +22,57 @@ export class Pumpchart extends Chart<PumpchartOptions> {
     };
     constructor(options: Partial<PumpchartOptions> = {}) {
         super(options, defaultPumpchartOptions);
-        // Set the size of the SVG element.
-        this.svg.setAttribute('viewBox', '0 0 ' + this.options.size.x + ' ' + this.options.size.y);
-        this.svg.setAttribute('width', this.options.size.x + 'px');
-        this.svg.setAttribute('height', this.options.size.y + 'px');
         // Append all groups to the SVG.
         Object.values(this.g).forEach(group => this.svg.appendChild(group));
+        // Set up frame of reference.
+        this.origin = {
+            x: this.options.padding.x,
+            y: this.options.size.y - this.options.padding.y,
+        };
+        this.farCorner = {
+            x: this.options.size.x - this.options.padding.x,
+            y: this.options.padding.y,
+        };
+        // Create the axes.
+        const xFlowAxis = this.createPath([
+            { flow: 0, head: 0 },
+            { flow: this.options.flow.max, head: 0 },
+        ]);
+        const yHeadAxis = this.createPath([
+            { flow: 0, head: 0 },
+            { flow: 0, head: this.options.head.max },
+        ]);
+        xFlowAxis.setAttribute('stroke', '#666');
+        yHeadAxis.setAttribute('stroke', '#666');
+        xFlowAxis.setAttribute('stroke-width', '2px');
+        yHeadAxis.setAttribute('stroke-width', '2px');
+        xFlowAxis.setAttribute('stroke-linecap', 'round');
+        yHeadAxis.setAttribute('stroke-linecap', 'round');
+        this.g.axes.append(xFlowAxis, yHeadAxis);
+    }
+    /**
+     * Convert a state to an (x,y) coordinate.
+     * @param state Any state in this system
+     * @returns An (x,y) coordinate on the screen
+     */
+    private state2xy(state: State): Point {
+        return {
+            x: SMath.translate(state.flow, 0, this.options.flow.max, this.origin.x, this.farCorner.x),
+            y: SMath.translate(state.head, 0, this.options.head.max, this.origin.y, this.farCorner.y),
+        };
+    }
+    /**
+     * Create a SVG path element from an array of states.
+     * @param data An array of states
+     * @param closePath Whether or not to close the path
+     * @returns A `<path>` element containing the array of states
+     */
+    private createPath(data: Array<State>, closePath: boolean = false): SVGPathElement {
+        const path: SVGPathElement = document.createElementNS(this.NS, 'path');
+        path.setAttribute('d', 'M ' + data.map(pt => {
+            const xy: Point = this.state2xy(pt);
+            return xy.x + ',' + xy.y;
+        }).join(' ') + (closePath ? ' z' : ''));
+        return path;
     }
 }
