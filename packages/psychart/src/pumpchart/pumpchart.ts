3166c3bedc328db0b0d08f8d1e07a7976876a2fa
Nicolas Ventura <ventura@lbl.gov>
2025-08-14 16:17:19 -0700

    Add more groups and set the size

--- packages/psychart/src/pumpchart/pumpchart.ts
+++ packages/psychart/src/pumpchart/pumpchart.ts
@@ -5,9 +5,16 @@ import { PumpchartOptions } from './types';
 export class Pumpchart extends Chart<PumpchartOptions> {
     private readonly g = {
         axes: document.createElementNS(this.NS, 'g'),
+        curves: document.createElementNS(this.NS, 'g'),
+        data: document.createElementNS(this.NS, 'g'),
     };
     constructor(options: Partial<PumpchartOptions> = {}) {
         super(options, defaultPumpchartOptions);
+        // Set the size of the SVG element.
+        this.svg.setAttribute('viewBox', '0 0 ' + this.options.size.x + ' ' + this.options.size.y);
+        this.svg.setAttribute('width', this.options.size.x + 'px');
+        this.svg.setAttribute('height', this.options.size.y + 'px');
+        // Append all groups to the SVG.
         Object.values(this.g).forEach(group => this.svg.appendChild(group));
     }
 }
