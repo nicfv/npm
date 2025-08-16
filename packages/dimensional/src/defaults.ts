68384e080f443e80c7c33a522aadfdef328e4c66
nicfv <github@nicfv.com>
2025-08-16 11:01:43 -0700

    Split into separate classes

--- packages/dimensional/src/defaults.ts
+++ packages/dimensional/src/defaults.ts
@@ -2,8 +2,7 @@ import { Dimension } from './dimension';
 import { Prefix } from './prefix';
 import { Unit } from './unit';
 
-class Defaults {
-    // ==== Prefixes ==== //
+class Prefixes {
     public readonly quetta = new Prefix('Q', 1e30);
     public readonly ronna = new Prefix('R', 1e27);
     public readonly yotta = new Prefix('Y', 1e24);
@@ -28,9 +27,15 @@ class Defaults {
     public readonly yocto = new Prefix('y', 1e-24);
     public readonly ronto = new Prefix('r', 1e-27);
     public readonly quecto = new Prefix('q', 1e-30);
+}
+/**
+ * Contains a set of default unit prefixes.
+ */
+export const prefixes = new Prefixes();
+
+class Dimensions {
     // ==== Special ==== //
     public readonly Dimensionless = new Dimension();
-    public readonly Unitless = new Unit();
     // ==== Dimensions ==== //
     public readonly Mass = new Dimension('\\textbf{M}');
     public readonly Length = new Dimension('\\textbf{L}');
@@ -49,20 +54,29 @@ class Defaults {
     public readonly pressure = this.force.over(this.area);
     public readonly charge = this.ElectricalCurrent.times(this.Time);
     public readonly voltage = this.energy.over(this.charge);
+}
+/**
+ * Contains a set of default base and derived dimensions.
+ */
+export const dimensions = new Dimensions();
+
+class Units {
+    // ==== Special ==== //
+    public readonly Unitless = new Unit();
     // ==== Base Units ==== //
-    public readonly gram = new Unit('g', this.Mass);
-    public readonly meter = new Unit('m', this.Length);
-    public readonly second = new Unit('s', this.Time);
-    public readonly kelvin = new Unit('K', this.Temperature);
-    public readonly ampere = new Unit('A', this.ElectricalCurrent);
-    public readonly candela = new Unit('cd', this.LuminousIntensity);
-    public readonly mole = new Unit('mol', this.AmountOfSubstance);
+    public readonly gram = new Unit('g', dimensions.Mass);
+    public readonly meter = new Unit('m', dimensions.Length);
+    public readonly second = new Unit('s', dimensions.Time);
+    public readonly kelvin = new Unit('K', dimensions.Temperature);
+    public readonly ampere = new Unit('A', dimensions.ElectricalCurrent);
+    public readonly candela = new Unit('cd', dimensions.LuminousIntensity);
+    public readonly mole = new Unit('mol', dimensions.AmountOfSubstance);
     // ==== Derived Units ==== //
     // Length
-    public readonly kilogram = this.gram.prefix(this.kilo);
-    public readonly kilometer = this.meter.prefix(this.kilo);
-    public readonly centimeter = this.meter.prefix(this.centi);
-    public readonly millimeter = this.meter.prefix(this.milli);
+    public readonly kilogram = this.gram.prefix(prefixes.kilo);
+    public readonly kilometer = this.meter.prefix(prefixes.kilo);
+    public readonly centimeter = this.meter.prefix(prefixes.centi);
+    public readonly millimeter = this.meter.prefix(prefixes.milli);
     public readonly foot = new Unit('ft', this.meter, 1 / 3.28);
     public readonly inch = new Unit('in', this.foot, 1 / 12);
     public readonly yard = new Unit('yd', this.foot, 3);
@@ -82,17 +96,25 @@ class Defaults {
     public readonly Fahrenheit = new Unit('^{\\circ}\\text{F}', this.Rankine);
     // Force
     public readonly Newton = new Unit('N', this.kilogram.times(this.meter).over(this.second.pow(2)));
-    public readonly kiloNewton = this.Newton.prefix(this.kilo);
+    public readonly kiloNewton = this.Newton.prefix(prefixes.kilo);
     // Energy
     public readonly Joule = new Unit('J', this.Newton.times(this.meter));
-    public readonly kiloJoule = this.Joule.prefix(this.kilo);
+    public readonly kiloJoule = this.Joule.prefix(prefixes.kilo);
+    // Power
+    public readonly watt = new Unit('W', this.Joule.over(this.second));
     // Pressure
     public readonly pascal = new Unit('Pa', this.Newton.over(this.meter.pow(2)));
-    public readonly kiloPascal = this.pascal.prefix(this.kilo);
+    public readonly kiloPascal = this.pascal.prefix(prefixes.kilo);
     public readonly atmosphere = new Unit('atm', this.pascal, 101325);
     public readonly bar = new Unit('bar', this.pascal, 1e5);
+    // Electrical
+    public readonly coulomb = new Unit('C', this.ampere.times(this.second));
+    public readonly volt = new Unit('V', this.Joule.over(this.coulomb));
+    public readonly ohm = new Unit('\\Omega', this.volt.over(this.ampere));
     // Other
     public readonly Gs = new Unit('G', this.meter.over(this.second.pow(2)), 9.81);
 }
-
-export const defaults = new Defaults();
+/**
+ * Contains a set of default base and derived units.
+ */
+export const units = new Units();
