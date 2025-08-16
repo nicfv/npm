545686c60f7eaa7371b3f19d9fedf6663ca81073
nicfv <github@nicfv.com>
2025-08-16 14:57:28 -0700

    Make testing work again

--- packages/dimensional/src/test.ts
+++ packages/dimensional/src/test.ts
@@ -1,51 +1,47 @@
 import * as T6 from 't6';
-import { defaults } from './defaults';
-import { Dimension } from './dimension';
-import { Prefix } from './prefix';
-import { Unit } from './unit';
+import { prefixes, dimensions, units, Prefix, Dimension, Unit } from '.';
 
 {
     // Dimension
     // .toString
-    T6.is(defaults.Dimensionless.toString(), '1');
-    T6.is(defaults.Mass.toString(), '{\\textbf{M}}');
-    T6.is(defaults.acceleration.toString(), '\\frac{{\\textbf{L}}}{{\\textbf{T}}^{2}}');
-    T6.is(defaults.Temperature.toString(), '{\\boldsymbol{\\Theta}}');
-    const customDimension = new Dimension('x').times(defaults.AmountOfSubstance).pow(2);
+    T6.is(dimensions.Dimensionless.toString(), '1');
+    T6.is(dimensions.Mass.toString(), '{\\textbf{M}}');
+    T6.is(dimensions.acceleration.toString(), '\\frac{{\\textbf{L}}}{{\\textbf{T}}^{2}}');
+    T6.is(dimensions.Temperature.toString(), '{\\boldsymbol{\\Theta}}');
+    const customDimension = new Dimension('x').times(dimensions.AmountOfSubstance).pow(2);
     T6.is(customDimension.toString(), '\\text{x}^{2} \\cdot {\\textbf{N}}^{2}');
     // .is
-    T6.isTrue(defaults.Dimensionless.is(new Dimension()));
-    T6.isTrue(defaults.velocity.is(defaults.Length.over(defaults.Time)));
-    T6.isTrue(defaults.force.is(defaults.Dimensionless.times(defaults.force)));
-    T6.isFalse(defaults.acceleration.is(defaults.Length.over(defaults.Time)));
-    T6.isFalse(customDimension.is(defaults.LuminousIntensity.pow(-2)));
+    T6.isTrue(dimensions.Dimensionless.is(new Dimension()));
+    T6.isTrue(dimensions.velocity.is(dimensions.Length.over(dimensions.Time)));
+    T6.isTrue(dimensions.force.is(dimensions.Dimensionless.times(dimensions.force)));
+    T6.isFalse(dimensions.acceleration.is(dimensions.Length.over(dimensions.Time)));
+    T6.isFalse(customDimension.is(dimensions.LuminousIntensity.pow(-2)));
     T6.isFalse(new Dimension('x').is(new Dimension('x'))); // These should be considered different dimensions
-    T6.isTrue(defaults.Dimensionless.over(defaults.Length).is(defaults.Length.pow(-1)));
+    T6.isTrue(dimensions.Dimensionless.over(dimensions.Length).is(dimensions.Length.pow(-1)));
 }
 
 {
     // Unit
     // .toString
-    console.log(T6.getTestNumber());
-    T6.is(defaults.kelvin.toString(), '\\text{K}');
-    T6.is(defaults.mile.toString(), '\\text{mi}');
-    T6.is(defaults.Rankine.toString(), '{^{\\circ}\\text{R}}');
-    T6.is(defaults.kilometer.toString(), '{\\text{k}\\text{m}}');
+    T6.is(units.kelvin.toString(), '\\text{K}');
+    T6.is(units.mile.toString(), '\\text{mi}');
+    T6.is(units.Rankine.toString(), '{^{\\circ}\\text{R}}');
+    T6.is(units.kilometer.toString(), '{\\text{k}\\text{m}}');
     // .dimensions
-    T6.isTrue(defaults.kelvin.dimensions.is(defaults.Temperature));
-    T6.isTrue(defaults.Newton.dimensions.is(defaults.force));
-    T6.isTrue(defaults.Newton.dimensions.is(defaults.Mass.times(defaults.acceleration)));
-    T6.isTrue(new Unit().dimensions.is(defaults.Dimensionless));
-    T6.isTrue(new Unit().is(defaults.Unitless));
-    T6.isTrue(new Unit('x').dimensions.is(defaults.Dimensionless)); // Unassigned base dimensions/units
+    T6.isTrue(units.kelvin.dimensions.is(dimensions.Temperature));
+    T6.isTrue(units.Newton.dimensions.is(dimensions.force));
+    T6.isTrue(units.Newton.dimensions.is(dimensions.Mass.times(dimensions.acceleration)));
+    T6.isTrue(new Unit().dimensions.is(dimensions.Dimensionless));
+    T6.isTrue(new Unit().is(units.Unitless));
+    T6.isTrue(new Unit('x').dimensions.is(dimensions.Dimensionless)); // Unassigned base dimensions/units
     // .prefix
-    const customKm = defaults.meter.prefix(defaults.kilo);
-    T6.is(customKm.toString(), defaults.kilometer.toString());
-    T6.isFalse(customKm.is(defaults.kilometer)); // Not seen as the same unit
+    const customKm = units.meter.prefix(prefixes.kilo);
+    T6.is(customKm.toString(), units.kilometer.toString());
+    T6.isFalse(customKm.is(units.kilometer)); // Not seen as the same unit
     let caught: boolean;
     caught = false;
     try {
-        customKm.prefix(defaults.milli);
+        customKm.prefix(prefixes.milli);
     } catch {
         caught = true;
     }
@@ -55,12 +51,12 @@ import { Unit } from './unit';
     // Customization
     const dimensionBlob = new Dimension('\\beta'),
         customPrefix = new Prefix('\\textbf{p}_{5}', 5),
-        customInch = defaults.inch.prefix(customPrefix),
+        customInch = units.inch.prefix(customPrefix),
         customUnit = customInch.over(new Unit('blob', dimensionBlob));
     T6.is(dimensionBlob.toString(), '{\\beta}');
     T6.is(customPrefix.LaTeX, '{\\textbf{p}_{5}}');
     T6.is(customInch.toString(), '{{\\textbf{p}_{5}}\\text{in}}');
     T6.is(customUnit.toString(), '\\frac{{{\\textbf{p}_{5}}\\text{in}}}{\\text{blob}}');
     T6.is(customUnit.dimensions.toString(), '\\frac{{\\textbf{L}}}{{\\beta}}');
-    T6.isTrue(customUnit.dimensions.is(defaults.Length.over(dimensionBlob)));
+    T6.isTrue(customUnit.dimensions.is(dimensions.Length.over(dimensionBlob)));
 }
