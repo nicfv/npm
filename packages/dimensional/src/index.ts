cf49cd04d570e017e04ca62a0bbbf727a13ae93b
Nic <github@nicfv.com>
2025-08-16 18:20:10 -0700

    So THIS is how you do it!!!

--- packages/dimensional/src/index.ts
+++ packages/dimensional/src/index.ts
@@ -5,7 +5,9 @@
  * ![NPM Downloads](https://img.shields.io/npm/d18m/dimensional)
  * ![NPM Last Update](https://img.shields.io/npm/last-update/dimensional)
  */
-export * from './defaults';
+export * as prefixes from './defaults/prefixes';
+export * as dimensions from './defaults/dimensions';
+export * as units from './defaults/units';
 export * from './dimension';
 export * from './prefix';
 export * from './quantity';
