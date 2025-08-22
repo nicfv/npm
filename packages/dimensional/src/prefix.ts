cdf2a06e016e63277e98d656859f136f623c44d0
nicfv <github@nicfv.com>
2025-08-21 18:31:32 -0700

    Revert "Don't wrap empty string in {}"
    This reverts commit e22d6451c03e4fb767c06a3509cc851fbb697d84.

--- packages/dimensional/src/prefix.ts
+++ packages/dimensional/src/prefix.ts
@@ -8,9 +8,7 @@ export class Prefix {
      * @param scale The scale of this prefix, relative to the base unit
      */
     constructor(public readonly LaTeX: string, public readonly scale: number) {
-        if (LaTeX === '') {
-            this.LaTeX = '';
-        } else if (/^[a-zA-Z]+$/.test(LaTeX)) {
+        if (/^[a-zA-Z]+$/.test(LaTeX)) {
             this.LaTeX = '\\text{' + LaTeX + '}';
         } else {
             this.LaTeX = '{' + LaTeX + '}';
