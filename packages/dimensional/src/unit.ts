1a0cffd1b60e72c5827e50da25dd452a5831c69e
nicfv <github@nicfv.com>
2025-08-21 18:30:34 -0700

    This was my hard worked attempt...

--- packages/dimensional/src/unit.ts
+++ packages/dimensional/src/unit.ts
@@ -1,3 +1,4 @@
+import { prefixes } from '.';
 import { Compound } from './compound';
 import { Dimension } from './dimension';
 import { Prefix } from './prefix';
@@ -20,8 +21,9 @@ export class Unit extends Compound<Unit> {
      * @param base The base dimension or makeup of unit(s)
      * @param scale The scale factor of this unit relative to the units in `base`
      * @param allowPrefix Whether or not this unit can have a prefix applied
+     * @param prefix The prefix of this unit, used for magnitude scaling
      */
-    constructor(LaTeXsymbol?: string | Map<Unit, number>, base?: Dimension | Unit, scale = 1, private readonly allowPrefix = false) {
+    constructor(LaTeXsymbol?: string | Map<Unit, number>, base?: Dimension | Unit, scale = 1, private readonly allowPrefix = true, private readonly prefix?: Prefix) {
         super(() => this, LaTeXsymbol);
         if (typeof LaTeXsymbol === 'string') {
             if (base instanceof Dimension) {
@@ -29,14 +31,14 @@ export class Unit extends Compound<Unit> {
                 this.dimensions = new Dimension().times(base);
             } else if (base instanceof Unit) {
                 // Initialize based on other unit(s)
-                this.scale = base.scale * scale;
+                this.scale = base.scale * (base.prefix?.scale ?? 1) * scale;
                 this.dimensions = new Dimension().times(base.dimensions);
             }
         } else if (LaTeXsymbol instanceof Map) {
             // Create a resultant unit based on other unit(s)
             for (const [factor, exponent] of LaTeXsymbol) {
                 this.dimensions = this.dimensions.times(factor.dimensions.pow(exponent));
-                this.scale *= (factor.scale ** exponent);
+                this.scale *= ((factor.scale * (factor.prefix?.scale ?? 1)) ** exponent);
             }
         }
     }
@@ -45,12 +47,18 @@ export class Unit extends Compound<Unit> {
      * @param prefix The prefix to apply
      * @returns A properly scaled unit
      */
-    public prefix(prefix: Prefix): Unit {
-        if (typeof this.LaTeX === 'string' && !this.allowPrefix) {
-            return new Unit(prefix.LaTeX + this.LaTeX, this, this.scale * prefix.scale, true);
-        } else {
-            throw new Error('Can only add a prefix to named base units.');
+    public pre(prefix: Prefix = prefixes.one): Unit {
+        if (typeof this.LaTeX !== 'string') {
+            throw new Error('Can only add a prefix to named units.');
         }
+        if (!this.allowPrefix) {
+            throw new Error('This unit cannot have a prefix.');
+        }
+        if (this.prefix) {
+            throw new Error('This unit already has a prefix.');
+        }
+        return new Unit(new Map([[this, 1]]), undefined, undefined, true, prefix);
+        // return new Unit(this.LaTeX, this.dimensions, this.scale, true, prefix);
     }
     /**
      * Calculate the conversion factor between this unit and another unit.
@@ -59,12 +67,26 @@ export class Unit extends Compound<Unit> {
      */
     public to(other: Unit): number {
         if (this.dimensions.is(other.dimensions)) {
-            return this.scale / other.scale;
+            return this.scale * (this.prefix?.scale ?? 1) / (other.scale * (other.prefix?.scale ?? 1));
         } else {
             throw new Error('Dimensions on ' + this.toString() + ' does not match dimensions on ' + other.toString() + '!');
         }
     }
     protected fromMap(factors: Map<Unit, number>): Unit {
         return new Unit(factors);
+    }
+    public override is(other: Unit): boolean {
+        return this.prefix === other.prefix && super.is(other);
+    }
+    /**
+     * Generate LaTeX code for rendering this unit.
+     * @returns A string representation of this unit
+     */
+    public override toString(): string {
+        if (this.prefix) {
+            return '{' + this.prefix.LaTeX + super.toString() + '}';
+        } else {
+            return super.toString();
+        }
     }
 }
