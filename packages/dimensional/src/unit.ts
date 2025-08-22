14aa7cee2bcc5be477389e7646312aeca8d79699
nicfv <github@nicfv.com>
2025-08-21 18:38:59 -0700

    Fix bug, probably?

--- packages/dimensional/src/unit.ts
+++ packages/dimensional/src/unit.ts
@@ -21,7 +21,7 @@ export class Unit extends Compound<Unit> {
      * @param scale The scale factor of this unit relative to the units in `base`
      * @param allowPrefix Whether or not this unit can have a prefix applied
      */
-    constructor(LaTeXsymbol?: string | Map<Unit, number>, base?: Dimension | Unit, scale = 1, private readonly allowPrefix = false) {
+    constructor(LaTeXsymbol?: string | Map<Unit, number>, base?: Dimension | Unit, scale = 1, private readonly allowPrefix = true) {
         super(() => this, LaTeXsymbol);
         if (typeof LaTeXsymbol === 'string') {
             if (base instanceof Dimension) {
@@ -46,8 +46,8 @@ export class Unit extends Compound<Unit> {
      * @returns A properly scaled unit
      */
     public prefix(prefix: Prefix): Unit {
-        if (typeof this.LaTeX === 'string' && !this.allowPrefix) {
-            return new Unit(prefix.LaTeX + this.LaTeX, this, this.scale * prefix.scale, true);
+        if (typeof this.LaTeX === 'string' && this.allowPrefix) {
+            return new Unit(prefix.LaTeX + this.LaTeX, this, this.scale * prefix.scale, false);
         } else {
             throw new Error('Can only add a prefix to named base units.');
         }
