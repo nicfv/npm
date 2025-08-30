18b2ae0a64bb5ad1f47b255804b2a9c34090bd1a
nicfv <github@nicfv.com>
2025-08-30 11:46:39 -0700

    Add prefix issue

--- packages/dimensional/example/Quirks.mjs
+++ packages/dimensional/example/Quirks.mjs
@@ -1,4 +1,4 @@
-import { Dimension, dimensions } from 'dimensional';
+import { Dimension, dimensions, prefixes, Quantity, units } from 'dimensional';
 
 // Define the length dimension
 const length_1 = new Dimension('L');
@@ -12,6 +12,25 @@ console.log(area.toString());
 // defined previously.
 const length_2 = new Dimension('L');
 console.log('length_1 == length_2?', length_1.is(length_2));
+
+// It's also not the same as the built-in dimension
+console.log('length_1 == length?', length_1.is(dimensions.Length));
+
+// By adding the same prefix to the same unit, but in 2
+// distinct instances, the units are no longer identical
+const km_1 = units.meter.prefix(prefixes.kilo);
+const km_2 = units.meter.prefix(prefixes.kilo);
+console.log('km_1 == km_2?', km_1.is(km_2));
+
+// However, their conversion factor is 1
+const km_to_km = km_1.to(km_2);
+console.log('Conversion factor:', km_to_km);
+
+// That means, quantities using either of
+// those units are technically identical
+const q1 = new Quantity(5, km_1);
+const q2 = q1.as(km_2); // Convert to km_2
+console.log(q1.toString(), q2.toString());
 
 // Let's derive a dimension using
 // pre-packaged default dimensions
