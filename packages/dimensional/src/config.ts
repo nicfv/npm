96cad1750b347c98a7b364e9fb3b35622ebd4da3
nicfv <github@nicfv.com>
2025-09-04 18:57:46 -0700

    Add config defaults

--- packages/dimensional/src/config.ts
+++ packages/dimensional/src/config.ts
@@ -3,19 +3,23 @@
  */
 export const config = {
     /**
-     * Automatically convert dimension, unit, and prefix names to LaTeX `text` elements
+     * Automatically convert dimension, unit, and prefix names to LaTeX `text` elements\
+     * Default: `true`
      */
     convertToText: true,
     /**
-     * The symbol to use for dimensionless and unitless quantities
+     * The symbol to use for dimensionless and unitless quantities\
+     * Default: `1`
      */
     scalarSymbol: '1',
     /**
-     * The LaTeX symbol for combined units or dimensions by multiplication
+     * The LaTeX symbol for combined units or dimensions by multiplication\
+     * Default: `\cdot`
      */
     multiplySymbol: '\\cdot',
     /**
-     * The number of decimals displayed when converting quantities to strings
+     * The number of decimals displayed when converting quantities to strings\
+     * Default: `3`
      */
     decimalsShown: 3,
     /**
@@ -23,11 +27,13 @@ export const config = {
      */
     unitDelimiters: {
         /**
-         * The left side, e.g. `[`
+         * The left side\
+         * Default: `\left[`
          */
         left: '\\left[',
         /**
-         * The right side, e.g. `]`
+         * The right side\
+         * Default: `\right]`
          */
         right: '\\right]',
     },
