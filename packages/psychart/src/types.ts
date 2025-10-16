c04533e7ad4059a5cd5d68f847540bd88b188c07
Nicolas Ventura <ventura@lbl.gov>
2025-10-16 16:33:33 -0700

    Add font and text anchor

--- packages/psychart/src/types.ts
+++ packages/psychart/src/types.ts
@@ -20,4 +20,59 @@ export interface ChartOptions {
      * The outer size of this chart, in pixels.
      */
     readonly size: Point;
+    /**
+     * The font used in this chart.
+     */
+    readonly font: {
+        /**
+         * The name of the font family.
+         */
+        readonly name: string;
+        /**
+         * The size of the font, in pixels.
+         */
+        readonly size: number;
+    };
+}
+
+/**
+ * Defines where the anchor is on a `<text>` element.
+ */
+export const enum TextAnchor {
+    /**
+     * Centered
+     */
+    C,
+    /**
+     * Northwest (upper-left)
+     */
+    NW,
+    /**
+     * North (upper-centered)
+     */
+    N,
+    /**
+     * Northeast (upper-right)
+     */
+    NE,
+    /**
+     * East (right-centered)
+     */
+    E,
+    /**
+     * Southeast (lower-right)
+     */
+    SE,
+    /**
+     * South (lower-centered)
+     */
+    S,
+    /**
+     * Southwest (lower-left)
+     */
+    SW,
+    /**
+     * West (left-centered)
+     */
+    W,
 }
