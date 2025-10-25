3db6020b7685ea45f5a719d87906d46e4eb5cf9b
Nic <github@nicfv.com>
2025-10-25 14:12:28 -0700

    start working on example

---
+++ packages/graphico/example/Test/src/index.js
@@ -0,0 +1,6 @@
+// import * as x from 'graphico';
+
+// const canvas = new x.Canvas({ parent: document.body });
+// canvas.graphics.fillStyle = '#ff0000';
+// canvas.graphics.fillRect(10, 20, 30, 40);
+console.log('test');
