2ed775c4f5ed64770dfdf392426102b0ada29c77
Nic <github@nicfv.com>
2025-10-25 12:37:26 -0700

    go back to @packageDocumentation

--- packages/graphico/src/index.ts
+++ packages/graphico/src/index.ts
@@ -1,9 +1,9 @@
 /**
+ * @packageDocumentation
  * Canvas 2D rendering toolkit for games and visual projects
  * 
  * ![NPM Downloads](https://img.shields.io/npm/d18m/graphico)
  * ![NPM Last Update](https://img.shields.io/npm/last-update/graphico)
- * @module
  */
 /**
  * Package Name
