b1bdca6b7961a835ce9e6ee35b07058e08c667b4
Nic <github@nicfv.com>
2025-10-25 21:56:39 -0700

    update example

--- packages/graphico/example/Test/src/index.js
+++ packages/graphico/example/Test/src/index.js
@@ -1,5 +1,14 @@
 import { Canvas } from 'graphico';
+import { Color } from 'viridis';
 
-const canvas = new Canvas();
-canvas.graphics.fillStyle = '#ff0000';
-canvas.graphics.fillRect(10, 20, 30, 40);
+const canvas = new Canvas({
+    border: new Color(0, 0, 0),
+    width: 60,
+    height: 40,
+    scale: 10,
+});
+canvas.graphics.strokeStyle = '#f00000';
+canvas.graphics.lineWidth = 5;
+canvas.graphics.moveTo(10, 20);
+canvas.graphics.lineTo(30, 50);
+canvas.graphics.stroke();
