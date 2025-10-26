5928cceef30db4c55c3c086b2cd0f8e3a2857321
Nic <github@nicfv.com>
2025-10-25 22:38:43 -0700

    work more on example

--- packages/graphico/example/Test/src/index.js
+++ packages/graphico/example/Test/src/index.js
@@ -2,6 +2,7 @@ import { Canvas } from 'graphico';
 import { Color } from 'viridis';
 
 const canvas = new Canvas({
+    background: new Color(99, 111, 222),
     border: new Color(0, 0, 0),
     width: 60,
     height: 40,
@@ -12,3 +13,9 @@ canvas.graphics.lineWidth = 5;
 canvas.graphics.moveTo(10, 20);
 canvas.graphics.lineTo(30, 50);
 canvas.graphics.stroke();
+
+const img = new Image(); // Create new img element
+img.src = "data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw==";
+canvas.graphics.drawImage(img, 0, 0);
+
+// canvas.clear();
