a128375fb9024fbd728ae39ce0a116daead2e638
Nic <github@nicfv.com>
2025-10-25 21:55:51 -0700

    set custom properties

--- packages/graphico/src/canvas.ts
+++ packages/graphico/src/canvas.ts
@@ -1,3 +1,5 @@
+import { Color } from 'viridis';
+
 /**
  * Represents a canvas for drawing and animating
  */
@@ -7,6 +9,11 @@ export class Canvas {
      */
     private static readonly defaults: Options = {
         parent: document.body,
+        width: 600,
+        height: 400,
+        scale: 1,
+        background: new Color(0, 0, 0, 0),
+        border: new Color(0, 0, 0, 0),
     };
     /**
      * Configuration options for this canvas
@@ -29,7 +36,19 @@ export class Canvas {
         } else {
             throw new Error('Could not initialize canvas graphics.');
         }
+        // Set static properties
         this.config.parent.appendChild(canvas);
+        canvas.tabIndex = 1;
+        canvas.focus();
+        canvas.style.outline = 'none';
+        canvas.style.imageRendering = 'pixelated';
+        // Set custom properties
+        canvas.width = this.config.width;
+        canvas.height = this.config.height;
+        canvas.style.width = `${this.config.width * this.config.scale}px`;
+        canvas.style.height = `${this.config.height * this.config.scale}px`;
+        canvas.style.background = this.config.background.toString();
+        canvas.style.border = `1px solid ${this.config.border}`;
     }
     /**
      * Set defaults for all undefined options.
@@ -51,4 +70,24 @@ export interface Options {
      * Appends the canvas onto the parent element
      */
     readonly parent: Node;
+    /**
+     * The width of the drawing area, in pixels
+     */
+    readonly width: number;
+    /**
+     * The height of the drawing area, in pixels
+     */
+    readonly height: number;
+    /**
+     * The scale of the drawing area to the actual size of the canvas element
+     */
+    readonly scale: number;
+    /**
+     * The background color of the canvas
+     */
+    readonly background: Color;
+    /**
+     * The CSS border style for the canvas
+     */
+    readonly border: Color;
 }
