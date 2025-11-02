d897e9f60193bf903d540d41ef90e3d22ce6912d
Nic <github@nicfv.com>
2025-11-01 23:03:21 -0700

    add instructions, reset

--- packages/graphico/example/Animation/src/index.js
+++ packages/graphico/example/Animation/src/index.js
@@ -5,11 +5,11 @@ const G = 9.81; // Gravitation constant [m/s^2]
 
 class Ball {
     #color;
-    #x;
-    #r;
-    #y;
-    #vy;
-    #groundY;
+    #x; // [px]
+    #r; // [px]
+    #y; // [px]
+    #vy; // [px/s]
+    #groundY; // [px]
     constructor(color = '', radius = 0, x = 0, dropHeight = 0, groundY = 0) {
         this.#color = color;
         this.#x = x;
@@ -17,6 +17,10 @@ class Ball {
         this.#y = groundY - dropHeight;
         this.#vy = 0;
         this.#groundY = groundY;
+    }
+    reset(dropHeight = 0) {
+        this.#y = this.#groundY - dropHeight;
+        this.#vy = 0;
     }
     step(dt) {
         const dt_s = dt / 1000; // convert ms to s
@@ -45,17 +49,37 @@ class Ground {
         this.#y = y;
     }
     draw(ctx) {
-        ctx.strokeStyle = 'gray';
-        ctx.lineWidth = 2;
-        ctx.beginPath();
-        ctx.moveTo(0, this.#y);
-        ctx.lineTo(1000, this.#y);
-        ctx.stroke();
+        ctx.fillStyle = 'sandybrown';
+        ctx.fillRect(0, this.#y, 1000, 1000);
+    }
+}
+
+class Instructions {
+    #content;
+    #x;
+    #y;
+    #show;
+    constructor(content = '', x = 0, y = 0) {
+        this.#content = content;
+        this.#x = x;
+        this.#y = y;
+        this.#show = true;
+    }
+    hide() {
+        this.#show = false;
+    }
+    draw(ctx) {
+        if (this.#show) {
+            ctx.fillStyle = 'black';
+            ctx.font = '12px sans-serif';
+            ctx.fillText(this.#content, this.#x, this.#y);
+        }
     }
 }
 
 const ball = new Ball('red', 10, 50, 200, 300);
 const ground = new Ground(300);
+const instructions = new Instructions('Press space to drop ball', 400, 20);
 
 const canvas = new Canvas({
     // debug: true,
@@ -64,6 +88,12 @@ const canvas = new Canvas({
     loop(dt) {
         ball.step(dt);
         canvas.clear();
-        return [ground, ball];
+        return [ground, ball, instructions];
     },
+    keydown(key) {
+        if (key === ' ') {
+            ball.reset(200);
+            instructions.hide();
+        }
+    }
 });
