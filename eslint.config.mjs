b9edb932512074678d9d903ae0e049b2b4da15f3
Nicolas Ventura <ventura@lbl.gov>
2025-11-11 15:09:46 -0800

    update eslint version, and now I can remove the config line due to the bug that was fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,9 +6,4 @@ export default defineConfig(
     eslint.configs.recommended,
     tseslint.configs.strict,
     tseslint.configs.stylistic,
-    { // https://github.com/eslint/eslint/issues/20272
-        rules: {
-            '@typescript-eslint/unified-signatures': 'off',
-        },
-    },
 );
