f3e0338ca1509736b6b76ed29bc9fa0f5b136115
Nicolas Ventura <ventura@lbl.gov>
2025-11-25 15:46:25 -0800

    Add options for pump flow/head and system flow/head

--- packages/psychart/src/pumpchart/types.ts
+++ packages/psychart/src/pumpchart/types.ts
@@ -57,4 +57,28 @@ export interface PumpchartOptions extends ChartOptions {
      * The axis thickness, in pixels
      */
     readonly axisWidth: number;
+    /**
+     * The maximum pump flow at no head pressure (used to build the performance curve)
+     */
+    readonly pumpMaxFlow: number;
+    /**
+     * The pump head or differential pressure at no flow (used to build the performance curve)
+     */
+    readonly pumpMaxHead: number;
+    /**
+     * The system head at no flow, or the difference between the inlet/outlet elevation (used to build the system curve)
+     */
+    readonly systemMinHead: number;
+    /**
+     * The flow rate at the operational point of the system (used to build the system curve)
+     */
+    readonly systemOpFlow: number;
+    /**
+     * The number of intermediate performance curves to generate
+     * @example
+     * A value of **1** would only generate the performance curve at 100%
+     * A value of **2** would generate the performances at 50% and 100%
+     * A value of **4** would generate performances at 25%, 50%, 75%, and 100%
+     */
+    readonly performanceSteps: number;
 }
