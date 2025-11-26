a0f0252418655d835e8ed5fe957eb26e314aa4fd
Nicolas Ventura <ventura@lbl.gov>
2025-11-26 14:17:54 -0800

    Update example

--- packages/psychart/example/Pump Curve/src/index.js
+++ packages/psychart/example/Pump Curve/src/index.js
@@ -1,6 +1,7 @@
 import { Pumpchart } from 'psychart';
 
 const pumpchart = new Pumpchart();
+pumpchart.plot({ flow: 50, head: 40 });
 
 window.addEventListener('load', () => {
     document.body.append(pumpchart.getElement());
