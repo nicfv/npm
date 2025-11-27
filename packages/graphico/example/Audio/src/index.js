004682784d9626455b0a12cb814871ddff5f13df
Nic <github@nicfv.com>
2025-11-26 17:27:54 -0800

    add example with playing audio

--- packages/graphico/example/Audio/src/index.js
+++ packages/graphico/example/Audio/src/index.js
@@ -29,7 +29,11 @@ class Button {
     }
     mouseClick(btn = 0) {
         // Make sure that the cursor is over the button and the left mouse button was pressed
-        this.#isDown = (this.#isHover && btn === 0);
+        if (this.#isHover && btn === 0 && !this.#isDown) {
+            this.#isDown = true;
+            // Play an audio file
+            canv.playAudio('https://www.wavsource.com/snds_2020-10-01_3728627494378403/sfx/blurp_x.wav');
+        }
     }
     mouseRelease(btn = 0) {
         if (this.#isDown && btn === 0) {
@@ -37,7 +41,6 @@ class Button {
         }
     }
     draw(ctx) {
-        if (!(ctx instanceof CanvasRenderingContext2D)) { return; }
         // Draw the background
         ctx.fillStyle = this.#backcolor;
         ctx.beginPath();
@@ -48,6 +51,28 @@ class Button {
         ctx.beginPath();
         ctx.ellipse(this.#x, this.#y + (this.#isDown ? this.#h : 0), this.#rx, this.#ry, 0, 0, 2 * Math.PI);
         ctx.fill();
+    }
+}
+
+class SoundStatus {
+    #x;
+    #y;
+    #font;
+    constructor(x = 0, y = 0, font = '20px sans-serif') {
+        this.#x = x;
+        this.#y = y;
+        this.#font = font;
+    }
+    draw(ctx) {
+        ctx.font = this.#font;
+        // Determine if the canvas is currently muted
+        if (canv.isMuted()) {
+            ctx.fillStyle = 'red';
+            ctx.fillText('Sound off', this.#x, this.#y);
+        } else {
+            ctx.fillStyle = 'black';
+            ctx.fillText('Sound on', this.#x, this.#y);
+        }
     }
 }
 
@@ -64,10 +89,21 @@ const canv = new Canvas({
     mouseup(button) {
         btn.mouseRelease(button);
     },
+    keydown(key) {
+        if (key === 'm') {
+            if (canv.isMuted()) {
+                canv.unmute();
+            } else {
+                canv.mute();
+            }
+        }
+    },
     loop(dt) {
         canv.clear();
         canv.draw(btn);
+        canv.draw(soundStatus);
     },
 });
 
 const btn = new Button(canv.width / 2, canv.height / 2);
+const soundStatus = new SoundStatus(10, 30);
