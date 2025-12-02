4c1e82e5c5393b837df8750072eb675dbb5076a6
Nicolas Ventura <ventura@lbl.gov>
2025-12-02 15:09:58 -0800

    write a function that finds the zeroes of any function

---
+++ packages/psychart/src/pumpchart/lib.ts
@@ -0,0 +1,28 @@
+/**
+ * Represents a mathematical function `h = f(q)`
+ */
+export type f = (q: number) => number;
+/**
+ * Find the solution (zero) of a function.
+ */
+export function zero(func: f, left: number, right: number, maxIterations = 1e6): number {
+    for (let i = 0; i < maxIterations; i++) {
+        const leftSign: number = Math.sign(func(left));
+        const rightSign: number = Math.sign(func(right));
+        if (leftSign === rightSign) {
+            throw new Error('A solution cannot be found.');
+        }
+        const mid: number = (left + right) / 2;
+        const midSign: number = Math.sign(func(mid));
+        if (leftSign === 0) { return left; }
+        if (rightSign === 0) { return right; }
+        if (midSign === 0) { return mid };
+        if (i > maxIterations) { return mid; }
+        if (midSign === leftSign) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+    return left;
+}
