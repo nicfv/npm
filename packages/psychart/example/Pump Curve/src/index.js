144370aa2045cd1d6566b1099aa459726b65b602
Nicolas Ventura <ventura@lbl.gov>
2025-12-05 15:23:02 -0800

    plot random data

--- packages/psychart/example/Pump Curve/src/index.js
+++ packages/psychart/example/Pump Curve/src/index.js
@@ -1,7 +1,40 @@
 import { Pumpchart } from 'psychart';
 
-const pumpchart = new Pumpchart();
-pumpchart.plot({ flow: 50, head: 40 });
+// Define timestamps
+const now = Date.now();
+const onehr = 60 * 60 * 1000;
+const in1hr = now + onehr
+const nData = 100;
+
+// Create a custom pump chart
+const pumpchart = new Pumpchart({
+    pumpCurve: '30-(q/100)^2',
+    systemCurve: '5+(q/75)^2',
+    speed: {
+        max: 60,
+        operation: 55,
+        steps: [15, 30, 45],
+    },
+    units: {
+        flow: 'gpm',
+        head: 'psi',
+        power: 'kW',
+        speed: 'rpm',
+    },
+});
+
+// Plot `nData` number of data points
+for (let t = now; t < in1hr; t += onehr / nData) {
+    const flow = Math.random() * 300;
+    const head = Math.random() * 20;
+    pumpchart.plot({ flow: flow, head: head }, {
+        timestamp: t,
+        gradient: {
+            name: 'Viridis',
+            score: (t - now) / onehr,
+        },
+    });
+}
 
 window.addEventListener('load', () => {
     document.body.append(pumpchart.getElement());
