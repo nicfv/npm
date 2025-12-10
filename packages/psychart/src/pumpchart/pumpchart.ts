e8dacd4807b0f92c0eda301cb0a260abdea6b41b
Nicolas Ventura <ventura@lbl.gov>
2025-12-10 11:23:55 -0800

    only show estimated speed if speed is not given

--- packages/psychart/src/pumpchart/pumpchart.ts
+++ packages/psychart/src/pumpchart/pumpchart.ts
@@ -248,8 +248,7 @@ export class Pumpchart extends Chart<PumpchartOptions> {
             (options.timestamp > 0 ? `${new Date(options.timestamp).toLocaleString()}\n` : '') +
             `Flow = ${SMath.round2(state.flow, 0.1)}${this.options.units.flow}` +
             `\nHead = ${SMath.round2(state.head, 0.1)}${this.options.units.head}` +
-            (speedEstimate > 0 ? `\nSpeed = ${SMath.round2(speedEstimate, 0.1)}${this.options.units.speed} (est.)` : '\nSpeed cannot be estimated') +
-            (typeof state.speed !== 'undefined' ? `\nSpeed = ${SMath.round2(state.speed, 0.1)}${this.options.units.speed}` : '') +
+            `\nSpeed = ${SMath.round2(state.speed ?? speedEstimate, 0.1)}${this.options.units.speed}${typeof state.speed === 'undefined' ? ' (est.)' : ''}` +
             (typeof state.power !== 'undefined' ? `\nPower = ${SMath.round2(state.power, 0.1)}${this.options.units.power}` : '');
         this.drawCircle(tip, color, state, options.radius, this.g.data);
     }
