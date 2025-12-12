700cbb6c9f24fb91484cd4d12bd96357b616ffb2
Nic <github@nicfv.com>
2025-12-12 15:03:34 -0800

    change to showId

--- packages/psychart/src/psychart/types.ts
+++ packages/psychart/src/psychart/types.ts
@@ -166,7 +166,7 @@ export interface DataOptions {
     /**
      * Optionally assign a numeric ID value to this point which will be displayed within the legend and Pumpchart.
      */
-    readonly id: number;
+    readonly showId: boolean;
     /**
      * The relative humidity measurement type, in percent [0-100] or float [0.0-1.0]
      */
