bf0c498e43adcd158f4964a2956737e81acbd9f0
Nic <github@nicfv.com>
2025-12-17 21:17:43 -0800

    simplify a bit

--- packages/psychart/example/Plotting Data/src/index.js
+++ packages/psychart/example/Plotting Data/src/index.js
@@ -1,17 +1,14 @@
 import { Psychart } from 'psychart';
 
 // Initialize Psychart.
-const ps = new Psychart({
+const psychart = new Psychart({
     // Let's render a few ASHRAE standard-55 psychrometric
     // envelopes for the current season onto our chart.
     regions: ['Summer (light work)', 'Summer (sitting)', 'Summer (walking)'],
 });
 
 // Append Psychart and the legend onto the document and plot data.
-window.addEventListener('load', () => {
-    document.body.append(ps.getElement());
-    plotData();
-});
+document.body.append(psychart.getElement());
 
 // Assume data comes in from room R1 as a clean CSV with the following columns:
 // [0]: Unix timestamp, [1]: Dry bulb (F), and [2]: Wet bulb (F)
@@ -35,73 +32,74 @@ const R2Data = [
 
 // Plot some data - this can be done dynamically
 // as data is received by the client.
-function plotData() {
-    // Knowing the start and end time of the measurements allows for gradients to be rendered properly
-    const startTime = 1700000000000,
-        endTime = 1700005000000;
-    for (const state of R1Data) {
-        // Extract the timestamp from the data table.
-        const timeStamp = state[0];
-        // Plot data onto Psychart.
-        ps.plot(
-            {
-                db: state[1],
-                other: state[2],
-                measurement: 'dbwb', // Shows the two measurement types are dry bulb and wet bulb
-            },
-            {
-                advanced: true,
-                line: true,
-                name: 'Room R1', // Need to assign a series name to connect data points and display in the legend
-                time: { start: startTime, end: endTime, now: timeStamp },
-            }
-        );
-    }
-    for (const state of R2Data) {
-        // Extract the timestamp from the data table.
-        const timeStamp = state[0];
-        // Plot data onto Psychart.
-        ps.plot(
-            {
-                db: state[1],
-                other: state[2],
-                measurement: 'dbdp' // Shows the two measurement types are dry bulb and dew point
-            },
-            {
-                advanced: true,
-                line: true,
-                name: 'Room R2', // Assign another series name to differentiate data series
-                time: { start: startTime, end: endTime, now: timeStamp },
-                gradient: 'Sunset', // Set a non-default gradient type to make it easier to visually differentiate between the two data series
-            }
-        );
-    }
-    // Plot the desired setpoint.
-    ps.plot(
+
+// Knowing the start and end time of the measurements allows for gradients to be rendered properly
+const startTime = 1700000000000,
+    endTime = 1700005000000;
+// Plot all data for room R1
+for (const state of R1Data) {
+    // Extract the timestamp from the data table.
+    const timeStamp = state[0];
+    // Plot data onto Psychart.
+    psychart.plot(
         {
-            db: 70,
-            other: 60,
-            measurement: 'dbwb'
+            db: state[1],
+            other: state[2],
+            measurement: 'dbwb', // Shows the two measurement types are dry bulb and wet bulb
         },
         {
-            name: 'Setpoint',
-            legend: false, // Show in the tooltip but not create a legend entry
-            color: '#DF1000', // Use a hex-code to define a single color instead of using a gradient
-            pointRadius: 4, // Enlarge the point to make it visually stand out
-        }
-    );
-    // Draw a warning line on the 70F wetbulb line, from 70-80F dry bulb.
-    ps.plot(
-        {
-            db: 80,
-            other: 70,
-            measurement: 'dbwb'
-        },
-        {
-            name: 'Warning',
-            legend: false,
-            color: 'FBAB10',
-            line: { db: 70, other: 70, measurement: 'dbwb' } // Draw a line between 2 arbitary points.
+            advanced: true,
+            line: true,
+            name: 'Room R1', // Need to assign a series name to connect data points and display in the legend
+            time: { start: startTime, end: endTime, now: timeStamp },
         }
     );
 }
+// Plot all data for room R2
+for (const state of R2Data) {
+    // Extract the timestamp from the data table.
+    const timeStamp = state[0];
+    // Plot data onto Psychart.
+    psychart.plot(
+        {
+            db: state[1],
+            other: state[2],
+            measurement: 'dbdp' // Shows the two measurement types are dry bulb and dew point
+        },
+        {
+            advanced: true,
+            line: true,
+            name: 'Room R2', // Assign another series name to differentiate data series
+            time: { start: startTime, end: endTime, now: timeStamp },
+            gradient: 'Sunset', // Set a non-default gradient type to make it easier to visually differentiate between the two data series
+        }
+    );
+}
+// Plot the desired setpoint.
+psychart.plot(
+    {
+        db: 70,
+        other: 60,
+        measurement: 'dbwb'
+    },
+    {
+        name: 'Setpoint',
+        legend: false, // Show in the tooltip but not create a legend entry
+        color: '#DF1000', // Use a hex-code to define a single color instead of using a gradient
+        pointRadius: 4, // Enlarge the point to make it visually stand out
+    }
+);
+// Draw a warning line on the 70F wetbulb line, from 70-80F dry bulb.
+psychart.plot(
+    {
+        db: 80,
+        other: 70,
+        measurement: 'dbwb'
+    },
+    {
+        name: 'Warning',
+        legend: false,
+        color: 'FBAB10',
+        line: { db: 70, other: 70, measurement: 'dbwb' } // Draw a line between 2 arbitary points.
+    }
+);
