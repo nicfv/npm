ffd45a57c1e06d952c67ce80a51cb3f1e754bdd9
nicfv <github@nicfv.com>
2026-06-07 16:52:32 -0700

    add more meta-tests

--- packages/t6/src/t6.test.ts
+++ packages/t6/src/t6.test.ts
@@ -6,17 +6,19 @@ T6.eq(1, 1);
 T6.ge(1, 1);
 T6.ge(2, 1);
 T6.gt(2, 1);
+T6.le(-2, 1);
+T6.lt(-2, 1);
 
 try {
     T6.isTrue(false);
 } catch (e) {
-    T6.is((e as Error).message, 'Exception found in test #7! The test returned false.');
+    T6.is((e as Error).message, 'Exception found in test #9! The test returned false.');
 }
 
 try {
     T6.ne(1, 1, 'Custom');
 } catch (e) {
-    T6.is((e as Error).message, 'Exception found in test #9! Custom');
+    T6.is((e as Error).message, 'Exception found in test #11! Custom');
 }
 
-T6.eq(T6.getTestNumber(), 10);
+T6.eq(T6.getTestNumber(), 12);
