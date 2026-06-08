d0f261e62fe6f211dc3773f270159f9e7c76ecc3
nicfv <github@nicfv.com>
2026-06-07 17:57:33 -0700

    finish vector tests

--- packages/smath/src/vec3.test.ts
+++ packages/smath/src/vec3.test.ts
@@ -33,10 +33,29 @@ T6.isTrue(b.equals(b));
 T6.isFalse(a.equals(b));
 T6.isFalse(b.equals(a));
 
+const a1: Vec3 = a.unit();
+T6.isTrue(SMath.approx(a1.x, 1 / Math.SQRT2));
+T6.isTrue(SMath.approx(a1.y, 1 / Math.SQRT2));
+T6.eq(a1.z, 0);
+T6.eq(a1.r, 1);
+T6.eq(a1.theta, a.theta);
+T6.eq(a1.phi, a.phi);
+
 const sum: Vec3 = a.plus(b);
+T6.isTrue(sum.equals(new Vec3(2 - Math.sqrt(3), 2, 1)));
+
 const diff: Vec3 = a.minus(b);
+T6.isTrue(diff.equals(new Vec3(2 + Math.sqrt(3), 2, -1)));
+
 const scaled: Vec3 = a.scaleBy(2);
+T6.isTrue(scaled.equals(new Vec3(4, 4, 0)));
+
 const cross1: Vec3 = a.cross(b);
 const cross2: Vec3 = b.cross(a);
-const dot: number = a.dot(b);
+T6.isTrue(cross1.equals(cross2.scaleBy(-1)));
+
+const dot: number = a.dot(a);
+T6.eq(dot, 8);
+
 const ab: Vec3 = a.projectOnto(b);
+T6.isTrue(ab.unit().scaleBy(-1).equals(b.unit(), 1e-6)); // Note: The projected vector could be pointing "backwards"
