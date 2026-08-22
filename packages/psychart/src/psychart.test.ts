d18f0d0e1c5afcabf4d72c3470a71b78b4e17ad8
nicfv <github@nicfv.com>
2026-08-21 17:19:30 -0700

    syntax for psychart tests look good

--- packages/psychart/src/psychart.test.ts
+++ packages/psychart/src/psychart.test.ts
@@ -4,95 +4,60 @@ import { Psychart } from './index.js';
 import { defaultOptions, regions } from './psychart/defaults.js';
 import { PsyState } from './psychart/psystate.js';
 
-T6.eq(Psychart.getRegionNamesAndTips().length, Object.entries(regions).length);
+describe('psychart static methods', () => {
+    it('get region names and tips', () => {
+        assert.strictEqual(Psychart.getRegionNamesAndTips().length, Object.entries(regions).length);
+    });
+});
 
-let caught: boolean;
+describe('psystate', () => {
+    describe('error catching', () => {
+        it('rh out of bounds', () => {
+            assert.throws(() => new PsyState({ db: 70, other: 1.60, measurement: 'dbrh' }, defaultOptions), /bounds/i);
+            assert.throws(() => new PsyState({ db: 70, other: -0.60, measurement: 'dbrh' }, defaultOptions), /bounds/i);
+        });
 
-caught = false;
-try {
-    new PsyState({ db: 70, other: 1.60, measurement: 'dbrh' }, defaultOptions);
-} catch {
-    caught = true;
-}
-T6.isTrue(caught, 'Uncaught RH was out of bounds.');
+        it('wb > db', () => {
+            assert.throws(() => new PsyState({ db: 70, other: 80, measurement: 'dbwb' }, defaultOptions), /bulb/i);
+            assert.throws(() => new PsyState({ db: 50, other: 51, measurement: 'dbwb' }, defaultOptions), /bulb/i);
+        });
 
-caught = false;
-try {
-    new PsyState({ db: 70, other: -0.60, measurement: 'dbrh' }, defaultOptions);
-} catch {
-    caught = true;
-}
-T6.isTrue(caught, 'Uncaught RH was out of bounds.');
+        it('invalid hr', () => {
+            assert.throws(() => new PsyState({ db: 60, other: -10e-3, measurement: 'dbhr' }, defaultOptions), /ratio/i);
+            assert.throws(() => new PsyState({ db: 60, other: 20e-3, measurement: 'dbhr' }, defaultOptions), /ratio/i);
+        });
 
-caught = false;
-try {
-    new PsyState({ db: 70, other: 80, measurement: 'dbwb' }, defaultOptions);
-} catch {
-    caught = true;
-}
-T6.isTrue(caught, 'Uncaught wb > db.');
+        it('valid hr', () => {
+            assert.ok(new PsyState({ db: 60, other: 10e-3, measurement: 'dbhr' }, defaultOptions));
+        });
 
-caught = false;
-try {
-    new PsyState({ db: 60, other: -10e-3, measurement: 'dbhr' }, defaultOptions);
-} catch {
-    caught = true;
-}
-T6.isTrue(caught, 'Uncaught invalid (low) humidity ratio');
+        it('invalid enthalpy', () => {
+            assert.throws(() => new PsyState({ db: 60, other: 10, measurement: 'dbh' }, defaultOptions), /ratio/i);
+            assert.throws(() => new PsyState({ db: 60, other: 30, measurement: 'dbh' }, defaultOptions), /ratio/i);
+        });
 
-caught = false;
-try {
-    new PsyState({ db: 60, other: 20e-3, measurement: 'dbhr' }, defaultOptions);
-} catch {
-    caught = true;
-}
-T6.isTrue(caught, 'Uncaught invalid (high) humidity ratio');
+        it('valid enthalpy', () => {
+            assert.ok(new PsyState({ db: 60, other: 20, measurement: 'dbh' }, defaultOptions));
+        });
+    });
 
-caught = false;
-try {
-    new PsyState({ db: 60, other: 10e-3, measurement: 'dbhr' }, defaultOptions);
-} catch (e) {
-    console.log(e);
-    caught = true;
-}
-T6.isFalse(caught, 'Caught valid humidity ratio');
-
-caught = false;
-try {
-    new PsyState({ db: 60, other: 10, measurement: 'dbh' }, defaultOptions);
-} catch {
-    caught = true;
-}
-T6.isTrue(caught, 'Uncaught invalid (low) enthalpy');
-
-caught = false;
-try {
-    new PsyState({ db: 60, other: 30, measurement: 'dbh' }, defaultOptions);
-} catch {
-    caught = true;
-}
-T6.isTrue(caught, 'Uncaught invalid (high) enthalpy');
-
-caught = false;
-try {
-    new PsyState({ db: 60, other: 20, measurement: 'dbh' }, defaultOptions);
-} catch {
-    caught = true;
-}
-T6.isFalse(caught, 'Caught valid enthalpy');
-
-// We will not be testing the accuracy of psychrolib, but will make sure that PsyState computes.
-const ps1: PsyState = new PsyState({ db: 70, other: 0.50, measurement: 'dbrh' }, defaultOptions);
-T6.eq(ps1.db, 70);
-T6.eq(ps1.rh, 0.50);
-// Just make sure that values are greater than zero.
-T6.gt(ps1.dp, 0);
-T6.gt(ps1.h, 0);
-T6.gt(ps1.hr, 0);
-T6.gt(ps1.s, 0);
-T6.gt(ps1.v, 0);
-T6.gt(ps1.vp, 0);
-T6.gt(ps1.wb, 0);
-const xy = ps1.toXY();
-T6.gt(xy.x, 0);
-T6.gt(xy.y, 0);
+    describe('valid psystate', () => {
+        it('test', () => {
+            // We will not be testing the accuracy of psychrolib, but will make sure that PsyState computes.
+            const ps1: PsyState = new PsyState({ db: 70, other: 0.50, measurement: 'dbrh' }, defaultOptions);
+            assert.strictEqual(ps1.db, 70);
+            assert.strictEqual(ps1.rh, 0.50);
+            // Just make sure that values are greater than zero.
+            assert.ok(ps1.dp > 0);
+            assert.ok(ps1.h > 0);
+            assert.ok(ps1.hr > 0);
+            assert.ok(ps1.s > 0);
+            assert.ok(ps1.v > 0);
+            assert.ok(ps1.vp > 0);
+            assert.ok(ps1.wb > 0);
+            const xy = ps1.toXY();
+            assert.ok(xy.x > 0);
+            assert.ok(xy.y > 0);
+        });
+    });
+});
