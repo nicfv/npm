93abf97d1be64848f774bc1f46996f7f9c177255
nicfv <github@nicfv.com>
2026-08-30 15:39:50 -0700

    ensure that tf is reached exactly

--- packages/smath/src/diffyq/single.ts
+++ packages/smath/src/diffyq/single.ts
@@ -1,3 +1,4 @@
+import { SMath } from '../index.js';
 import { DifferentialEquationBase } from './base.js';
 import { Equation, Step } from './types';
 
@@ -36,8 +37,9 @@ export class DifferentialEquation {
         }
         // Actually solve the equation
         while (this.DE.getTime() < tf) {
+            const dth: number = SMath.clamp(dt, 0, tf - this.DE.getTime());
             this.DE.setParams(...this.DE.getParams());
-            this.DE.step(dt);
+            this.DE.step(dth);
         }
         return this.DE.getData();
     }
