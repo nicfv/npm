043b96cf0766eb0e9cad08b5d86eef7fa349721d
nicfv <github@nicfv.com>
2026-08-30 08:50:40 -0700

    fix tests

--- packages/smath/src/de.test.ts
+++ packages/smath/src/de.test.ts
@@ -1,16 +1,18 @@
 import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
-import { DifferentialEquation, SMath } from './index.js';
+import { DifferentialEquation, SMath, Timeseries } from './index.js';
 
 describe('differential equations', () => {
     it('should solve y=e^x', () => {
-        const exp: DifferentialEquation = new DifferentialEquation((_t, x) => x, 1);
+        const exp: DifferentialEquation = new DifferentialEquation(1);
+        exp.set(() => exp.d(0), 1);
         exp.solve(1e-3, 4);
-        const t: number[] = exp.getTimeArray();
-        const y: number[] = exp.getNthDerivative(0);
         const e4: number = Math.E ** 4;
-        assert.equal(t.length, y.length);
-        assert.ok(SMath.approx(t[t.length - 1], 4, 0.1));
-        assert.ok(SMath.approx(y[y.length - 1], e4, 0.5));
+        const data: Timeseries<number>[] = exp.getTimeseries();
+        const last: Timeseries<number> = data[data.length - 1];
+        assert.equal(data.length, 4e3 + 2);
+        assert.ok(SMath.approx(last.time, 4, 0.01));
+        assert.ok(SMath.approx(last.data, e4, 0.1));
+        console.log(last, e4);
     });
 });
