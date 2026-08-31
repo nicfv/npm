8c5a009276bba58f5a965dada794212b9d0b5066
nicfv <github@nicfv.com>
2026-08-30 17:25:57 -0700

    docs

--- packages/smath/src/diffyq/single.ts
+++ packages/smath/src/diffyq/single.ts
@@ -40,7 +40,7 @@ export class DifferentialEquation {
         if (this.DE.getTime() > 0) {
             throw new Error('Solution already computed.');
         }
-        // Actually solve the equation
+        // For a single equation, the state vector is the local state of the equation
         while (this.DE.getTime() < tf) {
             const dth: number = SMath.clamp(dt, 0, tf - this.DE.getTime());
             this.DE.setState(...this.DE.getState());
